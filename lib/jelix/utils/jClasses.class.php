ae8d1f213a8dbee5c516835383a6e388e4b50f07
Loic Mathaud <loic@mathaud.net>
2006-05-04 13:05:23 +0000

    ajout de la méthode jClasses::inc() dans jClasses, permettant d'inclure une classe métier d'après un sélecteur
    --HG--
    extra : convert_revision : svn%3Ab2e90908-df08-0410-9efa-d317cf624d33/trunk%40158

--- lib/jelix/utils/jClasses.class.php
+++ lib/jelix/utils/jClasses.class.php
@@ -4,7 +4,7 @@
 * @subpackage  utils
 * @version     $Id$
 * @author      Jouanneau Laurent
-* @contributor
+* @contributor Loic Mathaud
 * @copyright   2005-2006 Jouanneau laurent
 * @link        http://www.jelix.org
 * @licence     GNU Lesser General Public Licence see LICENCE file or http://www.gnu.org/licenses/lgpl.html
@@ -12,37 +12,45 @@
 
 class jClasses {
 
-   static protected $_instances = array();
+    static protected $_instances = array();
 
-   private function __construct(){}
+    private function __construct(){}
 
-   static public function create($selector){
-
-      $sel = new jSelectorClass($selector);
-      if($sel->isValid()){
-        $p = $sel->getPath();
-        require_once ($p);
+    static public function create($selector){
+        $sel = self::_include($selector);
         $class = $sel->resource;
         return new $class ();
-      }else{
-         trigger_error(jLocale::get ('jelix~errors.selector.invalid', array ($selector)),E_USER_ERROR);
-      }
-   }
+    }
 
-   static public function createInstance($selector){
+    static public function createInstance($selector){
         return self::create($selector);
-   }
+    }
 
-   static public function getService($selector){
-       $sel = new jSelectorClass($selector);
-       if(isset(self::$_instances[$sel->toString()])){
-           return self::$_instances[$sel->toString()];
-       }else{
-           $o = self::create($selector);
-           self::$_instances[$sel->toString()]=$o;
-           return $o;
-       }
-   }
+    static public function getService($selector){
+        $sel = new jSelectorClass($selector);
+        if (isset(self::$_instances[$sel->toString()])) {
+            return self::$_instances[$sel->toString()];
+        } else {
+            $o = self::create($selector);
+            self::$_instances[$sel->toString()]=$o;
+            return $o;
+        }
+    }
+
+    static public function inc($selector) {
+        self::_include($selector);
+    }
+
+    static protected function _include($selector) {
+        $sel = new jSelectorClass($selector);
+        if ($sel->isValid()) {
+            $p = $sel->getPath();
+            require_once($p);
+            return $sel;
+        } else {
+            trigger_error(jLocale::get ('jelix~errors.selector.invalid', array ($selector)),E_USER_ERROR);
+        }
+    }
 }
 
 ?>
