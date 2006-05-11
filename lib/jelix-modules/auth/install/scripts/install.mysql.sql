1f05441c3950618cbf00ea042852820073518b40
Laurent Jouanneau <laurent@jelix.org>
2006-05-11 21:56:43 +0000

    corrections de syntaxe dans le fichier de conf du plugin auth
    --HG--
    extra : convert_revision : svn%3Ab2e90908-df08-0410-9efa-d317cf624d33/trunk%40169

--- lib/jelix-modules/auth/install/scripts/install.mysql.sql
+++ lib/jelix-modules/auth/install/scripts/install.mysql.sql
@@ -5,4 +5,5 @@ CREATE TABLE `jlx_user` (
   PRIMARY KEY  (`usr_login`)
 ) AUTO_INCREMENT=2 ;
 
+-- mot de passe : jelix
 INSERT INTO `jlx_user` VALUES ('admin', '893942cbbaf6dd55d6721353f6776df9', 'no@mail.com');
