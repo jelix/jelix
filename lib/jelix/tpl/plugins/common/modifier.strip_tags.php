6c707ba82499108b934e63c565615381726bf6e7
Laurent Jouanneau <laurent@jelix.org>
2006-08-01 08:12:43 +0000

    Plugin modifier : strip_tags (patch 1200)
    --HG--
    extra : convert_revision : svn%3Ab2e90908-df08-0410-9efa-d317cf624d33/trunk%40224

---
+++ lib/jelix/tpl/plugins/common/modifier.strip_tags.php
@@ -0,0 +1,19 @@
+<?php
+/**
+* @package    jelix
+* @subpackage template plugins
+* @version    $Id$
+* @author     Yannick Le Guédart
+* @contributor
+* @copyright   2006 Yannick Le Guédart
+* @link        http://www.jelix.org
+* @licence    GNU Lesser General Public Licence see LICENCE file or http://www.gnu.org/licenses/lgpl.html
+*/
+
+
+function jtpl_modifier_strip_tags($string)
+{
+    return strip_tags($string);
+}
+
+?>
