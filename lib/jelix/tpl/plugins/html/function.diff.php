f1d15ab0764abecbd786d694d399b3a4afdfefe3
Laurent Jouanneau <laurent@jelix.org>
2006-10-07 14:58:44 +0000

    ajout d'une lib diff et d'un plugin tpl {diff}
    --HG--
    extra : convert_revision : svn%3Ab2e90908-df08-0410-9efa-d317cf624d33/trunk%40278

---
+++ lib/jelix/tpl/plugins/html/function.diff.php
@@ -0,0 +1,34 @@
+<?php
+/**
+* @package    jelix
+* @subpackage jtpl_plugin
+* @version    $Id$
+* @author     Laurent Jouanneau
+* @copyright  2005-2006 Laurent Jouanneau
+* @link        http://www.jelix.org
+* @licence    GNU Lesser General Public Licence see LICENCE file or http://www.gnu.org/licenses/lgpl.html
+*/
+
+require_once(LIB_PATH.'diff/diffhtml.php');
+
+/**
+ * function plugin : show a diff between two string
+ *
+ * @param jTpl $tpl template engine
+ * @param string $str1 the first string
+ * @param string $str2 the second string
+ * @param string $nodiffmsg message quand il n'y a pas de différence
+ */
+function jtpl_function_diff($tpl, $str1,$str2, $nodiffmsg='Pas de différence';)
+{
+    $diff = new Diff(explode("\n",$str1),explode("\n",$str2));
+
+    if($diff->isEmpty()) {
+        echo $nodiffmsg;
+    }else{
+        $fmt = new HtmlUnifiedDiffFormatter();
+        echo $fmt->format($diff);
+    }
+}
+
+?>
