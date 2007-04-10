8ceef926257aadf1c2b38639d350cdcd978f1124
Laurent Jouanneau <laurent@jelix.org>
2007-04-10 10:46:09 +0000

    added the jIAuthDriverClass interface for classes to use with the 'class' auth driver. change API for this classes : it should provide a createUserObject() method, and findByLogin is changed to FindByLoginPattern
    --HG--
    extra : convert_revision : svn%3Ab2e90908-df08-0410-9efa-d317cf624d33/trunk%40416

--- lib/jelix/auth/jAuthDriverClass.class.php
+++ lib/jelix/auth/jAuthDriverClass.class.php
@@ -4,9 +4,80 @@
 * @subpackage auth
 * @author      Laurent Jouanneau
 * @contributor Yannick Le Guédart (adaptation de jAuthDriverDb pour une classe quelconque)
-* @copyright   2006 Laurent Jouanneau, 2006 Yannick Le Guédart
+* @copyright   2006-2007 Laurent Jouanneau, 2006 Yannick Le Guédart
 * @licence  http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public Licence, see LICENCE file
 */
+
+
+/**
+ * interface for classes used with the jAuthDriverClass
+* @package    jelix
+* @subpackage auth
+* @see jAuth
+* @since 1.0b2
+ */
+interface jIAuthDriverClass {
+    /**
+    * save a new user
+    * @param object $user user informations
+    */
+    public function insert($user);
+
+    /**
+    * delete a user
+    * @param string $login login of the user to delete
+    */
+    public function deleteByLogin($login);
+
+    /**
+    * update user informations
+    * @param object $user user informations
+    */
+    public function update($user);
+
+    /**
+    * get user informations
+    * @param string $login login of the user on which we want to get informations
+    * @return object user informations
+    */
+    public function getByLogin($login);
+
+    /**
+    * create an empty object which will contains user informations
+    * @return object user informations (empty)
+    */
+    public function createUserObject();
+
+    /**
+    * gets all users
+    * @return array list of users
+    */
+    public function findAll();
+
+    /**
+    * gets all users for which the login corresponds to the given pattern
+    * @param string $pattern the pattern
+    * @return array list of users
+    */
+    public function findByLoginPattern($pattern);
+
+    /**
+    * change the password of a user
+    * @param string $login the user login
+    * @param string $password the new encrypted password
+    */
+    public function updatePassword($login, $cryptedpassword);
+
+    /**
+    * get the user corresponding to the given login and encrypted password
+    * @param string $login the user login
+    * @param string $password the new encrypted password
+    * @return object user informations
+    */
+    public function getByLoginPassword($login, $cryptedpassword);
+}
+
+
 
 
 /**
@@ -48,7 +119,8 @@ class jAuthDriverClass implements jIAuthDriver {
     }
 
     public function createUserObject($login,$password){
-        $user = jClasses::createRecord($this->_params['class']);
+        $class = jClasses::create($this->_params['class']);
+        $user = $class->createUserObject();
         $user->login = $login;
         $user->password = $this->cryptPassword($password);
         return $user;
@@ -59,7 +131,7 @@ class jAuthDriverClass implements jIAuthDriver {
         if($pattern == '%' || $pattern == ''){
             return $class->findAll();
         }else{
-            return $class->findByLogin($pattern);
+            return $class->findByLoginPattern($pattern);
         }
     }
 
