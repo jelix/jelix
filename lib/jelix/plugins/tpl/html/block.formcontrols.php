82ad3646066d10e032f2b6cbca6aaf430a9ea26d
F. Fernandez <fernandez.no-mail@jelix.org>
2007-11-30 15:35:10 +0000

    enhancement #353: formcontrols plugin, added the possibility to indicate fields to hidden. p=F.Fernandez
    --HG--
    extra : convert_revision : svn%3Ab2e90908-df08-0410-9efa-d317cf624d33/trunk%40670

--- lib/jelix/plugins/tpl/html/block.formcontrols.php
+++ lib/jelix/plugins/tpl/html/block.formcontrols.php
@@ -3,8 +3,8 @@
 * @package     jelix
 * @subpackage  jtpl_plugin
 * @author      Jouanneau Laurent
-* @contributor Mickaël Fradin
-* @copyright   2007 Jouanneau laurent, 2007 Mickaël Fradin
+* @contributor Mickaël Fradin, F.Fernandez
+* @copyright   2007 Jouanneau laurent, 2007 Mickaël Fradin, 2007 F.Fernandez
 * @link        http://www.jelix.org
 * @licence     GNU Lesser General Public Licence see LICENCE file or http://www.gnu.org/licenses/lgpl.html
 */
@@ -33,26 +33,41 @@ function jtpl_block_html_formcontrols($compiler, $begin, $param=array())
         return '}} $t->_privateVars[\'__ctrlref\']=\'\';'; // if, foreach
     }
 
-    if(count($param) > 2){
-        $compiler->doError2('errors.tplplugin.block.bad.argument.number','formcontrols',2);
+    if(count($param) > 3){ 
+        $compiler->doError2('errors.tplplugin.block.bad.argument.number','formcontrols',3);
         return '';
     }
     if(count($param)){
         if(count($param) == 1){
             $content = 'if(is_array('.$param[0].')){
                 $ctrls_to_display = '.$param[0].';
+                $ctrls_notto_display = null; 
             }
             else {
                 $t->_privateVars[\'__form\'] = '.$param[0].';
                 $ctrls_to_display=null;
+                $ctrls_notto_display = null; 
+            }';
+        }
+        elseif(count($param) == 2){ 
+            $content = 'if(is_array('.$param[0].')){
+                $ctrls_to_display = '.$param[0].';
+                $ctrls_notto_display = '.$param[1].'; 
+            }
+            else {
+                $t->_privateVars[\'__form\'] = '.$param[0].';
+                $ctrls_to_display='.$param[1].';
+                $ctrls_notto_display = null; 
             }';
         }
         else{
             $content = ' $t->_privateVars[\'__form\'] = '.$param[0].";\n";
             $content .= ' $ctrls_to_display = '.$param[1].'; ';
+            $content .= ' $ctrls_notto_display = '.$param[2].'; ';
         }
     }else{
         $content = '$ctrls_to_display=null;';
+        $content .= '$ctrls_notto_display=null;';
     }
 
     $content .= '
@@ -62,7 +77,10 @@ if (!isset($t->_privateVars[\'__displayed_ctrl\'])) {
 $t->_privateVars[\'__ctrlref\']=\'\';
 foreach($t->_privateVars[\'__form\']->getControls() as $ctrlref=>$ctrl){ 
     if($ctrl->type == \'submit\' && isset($t->_privateVars[\'__formbuilder\'])) continue;
-    if(!isset($t->_privateVars[\'__displayed_ctrl\'][$ctrlref]) && ( $ctrls_to_display===null || in_array($ctrlref, $ctrls_to_display))){
+    if(!isset($t->_privateVars[\'__displayed_ctrl\'][$ctrlref])
+       && (  ($ctrls_to_display===null && $ctrls_notto_display === null)
+          || ($ctrls_to_display===null && !in_array($ctrlref, $ctrls_notto_display))
+          || in_array($ctrlref, $ctrls_to_display))) {
         $t->_privateVars[\'__ctrlref\'] = $ctrlref;
         $t->_privateVars[\'__ctrl\'] = $ctrl;
 ';
