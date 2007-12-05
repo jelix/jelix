3a72ea9c96f5d65730a9cc1d13ffbd9c51fab808
Christian Tritten <christian.tritten.no-mail@jelix.org>
2007-12-05 21:19:57 +0000

    ticket #340: improved the pagelinks plugin. p=Christian Tritten
    --HG--
    extra : convert_revision : svn%3Ab2e90908-df08-0410-9efa-d317cf624d33/trunk%40678

--- lib/jelix/plugins/tpl/html/function.pagelinks.php
+++ lib/jelix/plugins/tpl/html/function.pagelinks.php
@@ -4,6 +4,8 @@
 * @subpackage jtpl_plugin
 * @author     Jouanneau Laurent
 * @copyright  2007 Jouanneau laurent
+* @contributor Christian Tritten (christian.tritten@laposte.net)
+* @copyright  2007 Christian Tritten
 * @link        http://www.jelix.org
 * @licence    GNU Lesser General Public Licence see LICENCE file or http://www.gnu.org/licenses/lgpl.html
 */
@@ -18,57 +20,136 @@
  * @param integer $itemsTotal number of items
  * @param integer $offset  index of the first item to display
  * @param string $paramName name of the parameter in the actionParams which will content a page offset
- */
-function jtpl_function_html_pagelinks($tpl, $action, $actionParams, $itemsTotal, $offset, $pageSize=15, $paramName='offset' )
+ * @param array $displayProperties properties for the links display
+ *  */
+function jtpl_function_html_pagelinks($tpl, $action, $actionParams, $itemsTotal, $offset, $pageSize = 15, 
+                                      $paramName = 'offset', $displayProperties = array())
 {
-    $offset=intval($offset);
-    if($offset<=0)
-        $offset=0;
+    $offset = intval($offset);
+    if ($offset <= 0)
+        $offset = 0;
 
-    $itemsTotal=intval($itemsTotal);
+    $itemsTotal = intval($itemsTotal);
 
-    $pageSize=intval($pageSize);
-    if ($pageSize<1)
-        $pageSize=1;
+    $pageSize = intval($pageSize);
+    if ($pageSize < 1)
+        $pageSize = 1;
 
-    $urlaction = jUrl::get($action, $actionParams, jUrl::JURLACTION);
+    // If there are at least two pages of results
+    if ($itemsTotal > $pageSize) {
+        $jUrlEngine = jUrl::getEngine();
 
-    $pages = array(); // tableau des indices de toutes les pages
+        $urlaction = jUrl::get($action, $actionParams, jUrl::JURLACTION);
 
-    $current_page=1;
-    $pages[1]=0;
+        $defaultDisplayProperties = array('start-label' => '|&lt;',
+                                          'prev-label'  => '&lt;',
+                                          'next-label'  => '&gt;',
+                                          'end-label'   => '&gt;|',
+                                          'area-size'   => 0);
 
-    $numpage=1;
-    //$nextpage=0;
-    //$prevpage=0;
+        if (is_array($displayProperties) && count($displayProperties) > 0)
+            $displayProperties = array_merge($defaultDisplayProperties, $displayProperties);
+        else
+            $displayProperties = $defaultDisplayProperties;
 
-    // generates list of page offsets
-    for($curidx = 0; $curidx < $itemsTotal; $curidx += $pageSize){
-        if( $offset >= $curidx && $offset < $curidx + $pageSize){
-            //$current_page=$numpage;
-            //$nextpage=$numpage+1;
-            //$prevpage=$numpage-1;
-            echo ' <strong>'.$numpage.'</strong>';
-        }else{
-            $urlaction->params[$paramName] = $curidx;
-            $url = jUrl::getEngine()->create($urlaction);
-            echo ' <a href="'.$url->toString(true).'">'.$numpage.'</a>';
-            //$pages[$numpage++]=$curidx;
+        $pages = array();
+
+        $currentPage = 1;
+
+        $numpage = 1;
+
+        $prevBound = 0;
+
+        $nextBound = 0;
+
+        // Generates list of page offsets
+        for($curidx = 0; $curidx < $itemsTotal; $curidx += $pageSize) {
+            if($offset >= $curidx && $offset < $curidx + $pageSize) {
+                $pages[$numpage] = '<li class="pagelinks-current">'.$numpage.'</li>';
+                $prevBound = $curidx - $pageSize;
+                $nextBound = $curidx + $pageSize;
+                $currentPage = $numpage;
+            } else {
+                $urlaction->params[$paramName] = $curidx;
+                $url = $jUrlEngine->create($urlaction);
+                $pages[$numpage] = '<li><a href="'.$url->toString(true).'">'.$numpage.'</a></li>';
+            }
+            $numpage++;
         }
-        $numpage++;
+
+        // Calculate start page url
+        $urlaction->params[$paramName] = 0;
+        $urlStartPage = $jUrlEngine->create($urlaction);
+
+        // Calculate previous page url
+        $urlaction->params[$paramName] = $prevBound;
+        $urlPrevPage = $jUrlEngine->create($urlaction);
+
+        // Calculate next page url
+        $urlaction->params[$paramName] = $nextBound;
+        $urlNextPage = $jUrlEngine->create($urlaction);
+
+        // Calculate end page url
+        $urlaction->params[$paramName] = (count($pages) - 1) * $pageSize;
+        $urlEndPage = $jUrlEngine->create($urlaction);
+
+
+        // Links display 
+        echo '<ul class="pagelinks">';
+
+        // Start link
+        if (!empty($displayProperties['start-label'])) {
+            echo '<li class="pagelinks-start';
+            if ($prevBound >= 0) {
+                echo '"><a href="', $urlStartPage->toString(true), '">', $displayProperties['start-label'], '</a>';
+            } else {
+                echo ' pagelinks-disabled">',$displayProperties['start-label'] ;
+            }
+            echo '</li>', "\n";
+        }
+
+        // Previous link
+        if (!empty($displayProperties['prev-label'])) {
+            echo '<li class="pagelinks-prev';
+            if ($prevBound >= 0) {
+                echo '"><a href="', $urlPrevPage->toString(true), '">', $displayProperties['prev-label'], '</a>';
+            } else {
+                echo ' pagelinks-disabled">',$displayProperties['prev-label'] ;
+            }
+            echo '</li>', "\n";
+        }
+
+        // Pages links
+        foreach ($pages as $key => $page) {
+            if ($displayProperties['area-size'] == 0 || ($currentPage - $displayProperties['area-size'] <= $key) 
+                && ($currentPage + $displayProperties['area-size'] >= $key))
+                echo $page, "\n";
+        }
+
+        // Next link
+        if (!empty($displayProperties['next-label'])) {
+            echo '<li class="pagelinks-next';
+            if ($nextBound <= $itemsTotal) {
+                echo '"><a href="', $urlNextPage->toString(true), '">', $displayProperties['next-label'], '</a>';
+            } else {
+                echo ' pagelinks-disabled">',$displayProperties['next-label'] ;
+            }
+       	    echo '</li>', "\n";
+        }
+
+        // End link
+        if (!empty($displayProperties['end-label'])) {
+            echo '<li class="pagelinks-end';
+            if ($nextBound <= $itemsTotal) {
+                echo '"><a href="', $urlEndPage->toString(true), '">', $displayProperties['end-label'], '</a>';
+            } else {
+                echo ' pagelinks-disabled">',$displayProperties['end-label'] ;
+            }
+       	    echo '</li>', "\n";
+        }
+
+        echo '</ul>';
     }
-
-    /*
-    if(isset($pages[$nextpage]))
-        $next_page_idx = $pages[$nextpage];
-    else
-        $next_page_idx = -1;
-
-    if($current_page > 1)
-        $prev_page_idx = $offset - $page_size;
-    else
-        $prev_page_idx = -1;
-    */
 }
 
 ?>
