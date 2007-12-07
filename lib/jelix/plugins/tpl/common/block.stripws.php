ea99b025bf426984894882b54898d38b20a0063a
Hugues Magnier <hmagnier.no-mail@jelix.org>
2007-12-07 23:08:01 +0000

    ticket #140: added stripws tpl plugin to remove extra whitespace. p=Hugues Magnier
    --HG--
    extra : convert_revision : svn%3Ab2e90908-df08-0410-9efa-d317cf624d33/trunk%40687

---
+++ lib/jelix/plugins/tpl/common/block.stripws.php
@@ -0,0 +1,24 @@
+<?php
+/**
+* @package     jelix
+* @subpackage  plugin
+* @author      Hugues Magnier
+* @contributor
+* @copyright   2007 Hugues Magnier
+* @link        http://www.jelix.org
+* @licence     GNU Lesser General Public Licence see LICENCE file or http://www.gnu.org/licenses/lgpl.html
+*/
+
+function jtpl_block_common_stripws($compiler, $begin, $param=array()) {
+    if($begin){
+        $content = 'ob_start();';
+    }
+    else{
+        $content = '
+        $buffer = preg_replace(\'![\\t ]*[\\r\\n]+[\\t ]*!\', \'\', ob_get_contents());
+        ob_end_clean();
+        print $buffer;';
+    }
+    return $content;
+}
+?>
