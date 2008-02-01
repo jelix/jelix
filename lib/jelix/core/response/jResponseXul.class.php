329f6542ec433b3063bfca641d2b8abb1afd3662
Laurent Jouanneau <laurent@jelix.org>
2008-02-01 21:04:24 +0000

    fixed bug #444: js error when displaying log and errors with firebug, when the message contains some CR char
    --HG--
    extra : convert_revision : svn%3Ab2e90908-df08-0410-9efa-d317cf624d33/trunk%40758

--- lib/jelix/core/response/jResponseXul.class.php
+++ lib/jelix/core/response/jResponseXul.class.php
@@ -131,7 +131,7 @@ class jResponseXul extends jResponse {
                         echo 'console.error("[error ';
                         break;
                     }
-                    echo $e[1].'] '.str_replace('"','\"',$e[2]),' (',str_replace('\\','\\\\',$e[3]),' ',$e[4],')");';
+                    echo $e[1],'] ',str_replace(array('"',"\n","\r"),array('\"',' ',' '),$e[2]),' (',str_replace('\\','\\\\',$e[3]),' ',$e[4],')");';
                 }
                 echo '}else{alert("there are some errors, you should activate Firebug to see them");}</script>';
             }else{
@@ -154,7 +154,7 @@ class jResponseXul extends jResponse {
             if(count($GLOBALS['gJCoord']->logMessages['firebug'])) {
                 echo '<script type="text/javascript">if(console){';
                 foreach($GLOBALS['gJCoord']->logMessages['firebug'] as $m) {
-                    echo 'console.debug("',str_replace(array('\\','"'),array('\\\\','\"'),$m),'");';
+                    echo 'console.debug("',str_replace(array('\\','"',"\n","\r"),array('\\\\','\"',' ',' '),$m),'");';
                 }
                 echo '}else{alert("there are log messages, you should activate Firebug to see them");}</script>';
             }
