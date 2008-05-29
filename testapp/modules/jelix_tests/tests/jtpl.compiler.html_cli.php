ef60dc08a4f17c979d253debc367fac805fa7349
Laurent Jouanneau <laurent@jelix.org>
2008-05-29 08:27:20 +0000

    fixed a bug in a unit test, about the lastest modification in the form plugin. You MUST run unit tests before to propose a patch and before commiting a patch.
    --HG--
    extra : convert_revision : svn%3Ab2e90908-df08-0410-9efa-d317cf624d33/trunk%40958

--- testapp/modules/jelix_tests/tests/jtpl.compiler.html_cli.php
+++ testapp/modules/jelix_tests/tests/jtpl.compiler.html_cli.php
@@ -198,7 +198,7 @@ function toto() {
          3=>array('{for ($i=0;$i<$p;$i++)} A {/for}',
                   'jelix~errors.tpl.tag.character.invalid',array('for ($i=0;$i<$p;$i++)','(',null) ),
          4=>array('{form ($foo,$params)} aa {/form}',
-                  'jelix~errors.tplplugin.block.bad.argument.number',array('form','2-6',null) ),
+                  'jelix~errors.tplplugin.block.bad.argument.number',array('form','2-7',null) ),
          5=>array('{($aaa)}',
                   'jelix~errors.tpl.tag.syntax.invalid',array('($aaa)',null) ),
          );
