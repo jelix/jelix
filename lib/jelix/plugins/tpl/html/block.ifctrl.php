62df4121654ab3c46c4b3fb61f59b223a4b28b92
Dominique Papin <dominique.papin@ruedesecoles.com>
2008-05-31 15:12:01 +0000

    #601: error message was not renamed correctly.
    --HG--
    extra : convert_revision : svn%3Ab2e90908-df08-0410-9efa-d317cf624d33/trunk%40972

--- lib/jelix/plugins/tpl/html/block.ifctrl.php
+++ lib/jelix/plugins/tpl/html/block.ifctrl.php
@@ -12,7 +12,7 @@
  * a special if block to test easily the current control name
  * TO BE USED inside a {formcontrols} block
  *
- * {ifctrlname 'name1','name2',...}some tpl{else}some other tpl{/ifctrlname}
+ * {ifctrl 'name1','name2',...}some tpl{else}some other tpl{/ifctrl}
  * @param jTplCompiler $compiler the template compiler
  * @param boolean $begin true if it is the begin of block, else false
  * @param array $params 0=>'name',etc. to match against current control name
@@ -23,7 +23,7 @@ function jtpl_block_html_ifctrl($compiler, $begin, $params=array())
     if($begin){
         if(count($params)==0) {
             $content='';
-            $compiler->doError1('errors.tplplugin.block.bad.argument.number','ifctrlname', '1+');
+            $compiler->doError1('errors.tplplugin.block.bad.argument.number','ifctrl', '1+');
         } else {
             $content = ' if(isset($t->_privateVars[\'__ctrlref\'])&&(';
             foreach( $params as $ctrlname )
