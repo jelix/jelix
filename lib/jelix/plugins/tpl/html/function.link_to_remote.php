6a993795c0c66dc2ac5afe16f7a8ded33a8eaa21
Julien Jacottet <jjacottet.no-mail@jelix.org>
2008-06-10 14:42:49 +0000

    ticket #30: added link_to_remote plugin. p=Julien Jacottet
    --HG--
    extra : convert_revision : svn%3Ab2e90908-df08-0410-9efa-d317cf624d33/trunk%40982

---
+++ lib/jelix/plugins/tpl/html/function.link_to_remote.php
@@ -0,0 +1,70 @@
+<?php
+/**
+* @package     Jelix
+* @subpackage  jtpl_plugin
+* @author      Julien Jacottet
+* @contributor
+* @copyright   2008 Julien Jacottet
+* @link        http://www.jelix.org
+* @licence     GNU Lesser General Public Licence see LICENCE file or http://www.gnu.org/licenses/lgpl.html
+*/
+
+/**
+ * function plugin :  Ajax request
+ *
+ * it creates a javascript ajax function
+ * example : 
+ * <pre>
+ * {link_to_remote
+ *  'Link',    <!-- link label -->
+ *  'result',    <!-- id dom for ajax result -->
+ *  'test~default:ajax', array('id'=>'34'),    <!-- jurl request -->
+ *  array(
+ *    'position'=>'html',    <!-- html or append or prepend (default html) -->
+ *    'method'=>'GET',    <!-- GET or POST (default POST) -->
+ *    'beforeSend'=>'alert("beforeSend")',    <!-- JS script before send (default null) -->
+ *    'complete'=>'alert("complete")',    <!-- JS script after send  (default null)-->
+ *    'error'=>'alert("error")',    <!-- JS if error  (default null) -->
+ * )}
+ * <div id="result"></div>
+ * </pre>
+ */
+
+function jtpl_function_html_link_to_remote($tpl, $label, $element_id, $action_selector, $action_parameters, $option) {
+
+    global $gJCoord, $gJConfig;
+    static $id_link_to_remote = 0;
+
+    // Add js link 
+    $gJCoord->response->addJSLink($gJConfig->urlengine['basePath'].'jelix/jquery/jquery.js');
+
+    $id_link_to_remote++;
+
+    $url = jUrl::get($action_selector, $action_parameters);
+
+    $position = ((array_key_exists("position", $option)) ? $option['position'] : 'html' );
+    $method =   ((array_key_exists("method", $option)) ? $option['method'] : 'GET' );
+    $beforeSend = ((array_key_exists("beforeSend", $option)) ? $option['beforeSend'] : '' );
+    $complete = ((array_key_exists("complete", $option)) ? $option['complete'] : '' );
+    $error =    ((array_key_exists("error", $option)) ? $option['error'] : '' );
+
+    // Link
+    echo '<a href="#" onclick="link_to_remote_'.$id_link_to_remote.'();">'.$label."</a>\n";
+
+    // Script
+    echo '
+    <script>
+      function link_to_remote_'.$id_link_to_remote.'() {
+        $.ajax({
+          type: \''.$method."',
+          url: '".$url."',
+          beforeSend: function(){".$beforeSend.";},
+          complete: function(){".$complete.";},
+          error: function(){".$error.';},
+          success: function(msg){
+            $(\'#'.$element_id."').".$position."(msg);
+          }
+        });
+      };
+    </script>";
+}
