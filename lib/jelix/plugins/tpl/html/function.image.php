3b6880683e3d6d0c33896b83c2d370939fd07bac
Dominique Papin <dominique.papin@ruedesecoles.com>
2008-07-11 16:38:57 +0000

    #628: preserve aspectratio with maxwidth and maxheight attributes (plugin image)
    --HG--
    extra : convert_revision : svn%3Ab2e90908-df08-0410-9efa-d317cf624d33/trunk%401013

--- lib/jelix/plugins/tpl/html/function.image.php
+++ lib/jelix/plugins/tpl/html/function.image.php
@@ -1,16 +1,17 @@
 <?php
 /**
- * @package    jelix
- * @subpackage jtpl_plugin
- * @author     Lepeltier kévin
- * @copyright  2007-2008 Lepeltier kévin
- * @link       http://www.jelix.org
- * @licence    GNU Lesser General Public Licence see LICENCE file or http://www.gnu.org/licenses/lgpl.html
+ * @package     jelix
+ * @subpackage  jtpl_plugin
+ * @author      Lepeltier kévin
+ * @contributor Dominique Papin
+ * @copyright   2007-2008 Lepeltier kévin, 2008 Dominique Papin
+ * @link        http://www.jelix.org
+ * @licence     GNU Lesser General Public Licence see LICENCE file or http://www.gnu.org/licenses/lgpl.html
  */
 
 /**
  * image plugin :  write the url corresponding to the image
- * 
+ *
  * Add a link to the image,
  * The image is resized, and cached
  *
@@ -50,47 +51,47 @@
  * @param array $params parameters for the url
  */
 function jtpl_function_html_image($tpl, $src, $params=array()) {
-    
+
     // Extension
     if( empty($params['ext']) ) {
         $path_parts = pathinfo($src);
         $ext = strtolower($path_parts['extension']);
     } else $ext = strtolower($params['ext']);
-    
+
     // White background for IE
     if (   empty($params['background'])
         && strpos($_SERVER["HTTP_USER_AGENT"], 'MSIE') !== false
         && strpos($_SERVER["HTTP_USER_AGENT"], 'MSIE 7') === false) {
         $params['background'] = '#ffffff';
     }
-    
+
     // Name of the file cache
     $chaine = $src;
     foreach($params as $key => $value)
         if( !in_array($key, array('alt', 'class', 'id', 'style', 'longdesc', 'name', 'ismap', 'usemap', 'title', 'dir', 'lang', 'onclick', 'ondblclick', 'onmousedown', 'onmouseup', 'onmouseover', 'onmousemove', 'onmouseout', 'onkeypress', 'onkeydown', 'onkeyup')))
             $chaine .= $key.$value;
     $cachename = md5($chaine).'.'.$ext;
-    
+
     // Path
     $cache_path = JELIX_APP_WWW_PATH.'cache/images/'.$cachename;
     $origine_path = JELIX_APP_WWW_PATH.$src;
-    
+
     global $gJConfig;
     $www = 'http'.(empty($_SERVER['HTTPS'])?'':'s').'://'.$_SERVER['HTTP_HOST'].$gJConfig->urlengine['basePath'];
     $cache_www = $www.'cache/images/'.$cachename;
     $origine_www = $www.$src;
-    
+
     // Cache and make changes if necessary
     if( is_file($origine_path) && !is_file($cache_path) ) {
         $att = array('width'=>'', 'height'=>'', 'maxwidth'=>'', 'maxheight'=>'', 'zoom'=>'', 'alignh'=>'', 'alignv'=>'', 'ext'=>'', 'quality'=>'', 'shadow'=>'');
         if( count(array_intersect_key($params, $att)) )
             jtpl_function_html_image_inCache($src, $cachename, $params);
     }
-    
+
     // Attributes
     $att = array('alt'=>'', 'id'=>'', 'class'=>'', 'style'=>'', 'longdesc'=>'', 'name'=>'', 'ismap'=>'', 'usemap'=>'', 'title'=>'', 'dir'=>'', 'lang'=>'', 'onclick'=>'', 'ondblclick'=>'', 'onmousedown'=>'', 'onmouseup'=>'', 'onmouseover'=>'', 'onmousemove'=>'', 'onmouseout'=>'', 'onkeypress'=>'', 'onkeydown'=>'', 'onkeyup'=>'');
     $att = array_intersect_key($params, $att);
-    
+
     // If the image does not undergo transformation
     if( !is_file($cache_path) ) {
         $att['src'] = $origine_www;
@@ -101,29 +102,29 @@ function jtpl_function_html_image($tpl, $src, $params=array()) {
         else if( !empty($params['maxheight']) )    $att['style'] .= 'height: '.$params['maxheight'].'px;';
     } else
         $att['src'] = $cache_www;
-    
+
     // Tag image
     echo '<img';
     foreach( $att as $key => $val )
         if( !empty($val) )
             echo ' '.$key.'="'.$val.'"';
     echo '/>';
-    
+
     }
 
 function jtpl_function_html_image_inCache($src, $cachename, $array) {
-    
-    $mimes = array('gif'=>'image/gif', 'png'=>'image/png', 
-                   'jpeg'=>'image/jpeg', 'jpg'=>'image/jpeg', 'jpe'=>'image/jpeg', 
+
+    $mimes = array('gif'=>'image/gif', 'png'=>'image/png',
+                   'jpeg'=>'image/jpeg', 'jpg'=>'image/jpeg', 'jpe'=>'image/jpeg',
                    'xpm'=>'image/x-xpixmap', 'xbm'=>'image/x-xbitmap', 'wbmp'=>'image/vnd.wap.wbmp');
-    
+
     global $gJConfig;
     $origine_www = 'http'.(empty($_SERVER['HTTPS'])?'':'s').'://'.$_SERVER['HTTP_HOST'].$gJConfig->urlengine['basePath'].$src;
-    
+
     $path_parts = pathinfo($origine_www);
     $ext = $mimes[strtolower($path_parts['extension'])];
     $quality = (!empty($array['quality']))?  $array['quality'] : 100;
-    
+
     // Creating an image
     switch ( $ext ) {
         case 'image/gif'             : $image = imagecreatefromgif($origine_www); break;
@@ -134,29 +135,40 @@ function jtpl_function_html_image_inCache($src, $cachename, $array) {
         case 'image/x-xpixmap'       : $image = imagecreatefromxpm($origine_www); break;
         default                      : return ;
     }
-    
+
     if(!empty($array['maxwidth']) && !empty($array['maxheight'])) {
-        
-        $rapy = imagesy($image)/$array['maxwidth'];
-        $rapx = imagesx($image)/$array['maxheight'];
-        
-        if( $rapy > $rapx ) {
-            $array['height'] = $array['maxheight'];
-            $array['width'] = imagesx($image)/$rapy;
+
+        $origWidth = imagesx($image);
+        $origHeight = imagesy($image);
+        $constWidth = $array['maxwidth'];
+        $constHeight = $array['maxheight'];
+        $ratio = imagesx($image)/imagesy($image);
+
+        if ( $origWidth < $constWidth && $origHeight < $constHeight ) {
+            $array['width'] = $origWidth;
+            $array['height'] = $origHeight;
         } else {
-            $array['width'] = $array['maxwidth'];
-            $array['height'] = imagesy($image)/$rapx;
+            $ratioHeight = $constWidth/$ratio;
+            $ratioWidth = $constHeight*$ratio;
+            if ( $ratioWidth > $constWidth ) {
+                $constHeight = $ratioHeight;
+            }
+            else if ( $ratioHeight > $constHeight ) {
+                $constWidth = $ratioWidth;
+            }
+            $array['width'] = $constWidth;
+            $array['height'] = $constHeight;
         }
     }
-    
+
     if (!empty($array['width']) || !empty($array['height'])) {
-    
+
         $ancienimage = $image;
         $resampleheight = imagesy($ancienimage);
         $resamplewidth = imagesx($ancienimage);
         $posx = 0;
         $posy = 0;
-        
+
         if(empty($array['width'])) {
             $finalheight = $array['height'];
             $finalwidth = $finalheight*imagesx($ancienimage)/imagesy($ancienimage);
@@ -174,39 +186,39 @@ function jtpl_function_html_image_inCache($src, $cachename, $array) {
                 }
             }
         }
-        
+
         if(!empty($array['zoom'])) {
             $resampleheight /= 100/$array['zoom'];
             $resamplewidth /= 100/$array['zoom'];
         }
-        
+
         $posx = imagesx($ancienimage)/2 -$resamplewidth/2;
         $posy = imagesy($ancienimage)/2 -$resampleheight/2;
-        
+
         if(!empty($array['alignh'])) {
             if($array['alignh'] == 'left')            $posx = 0;
             else if($array['alignh'] == 'right')    $posx = -($resamplewidth - imagesx($ancienimage));
             else if($array['alignh'] != 'center')    $posx = -$array['alignh'];
         }
-        
+
         if(!empty($array['alignv'])) {
             if($array['alignv'] == 'top')            $posy = 0;
             else if($array['alignv'] == 'bottom')    $posy = -($resampleheight - imagesy($ancienimage));
             else if($array['alignv'] != 'center')    $posy = -$array['alignv'];
         }
-        
+
         $image = imagecreatetruecolor($finalwidth, $finalheight);
         imagesavealpha($image, true);
         $tp = imagecolorallocatealpha($image,0,0,0,127);
         imagefill($image,0,0,$tp);
-        
+
         imagecopyresampled($image, $ancienimage, 0, 0, $posx, $posy, imagesx($image), imagesy($image), $resamplewidth, $resampleheight);
     }
-    
+
     // The shadow cast adds to the dimension of the image chooses
     if( !empty($array['shadow']) )
        $image = jtpl_function_html_image_ombre ($image, $array);
-    
+
     // Background
     if( !empty($array['background']) ) {
         $array['background'] = str_replace('#', '', $array['background']);
@@ -217,33 +229,33 @@ function jtpl_function_html_image_inCache($src, $cachename, $array) {
         imagecopy( $fond, $image, 0, 0, 0, 0, imagesx($image), imagesy($image));
         $image = $fond;
     }
-    
-    
+
+
     $ext = empty($array['ext'])?$ext:$mimes[$array['ext']];
     $cache_path = JELIX_APP_WWW_PATH.'cache/images/';
     jFile::createDir($cache_path);
-    
-    
+
+
     // Register
     switch ( $ext ) {
         case 'image/gif'  : imagegif($image, $cache_path.$cachename); break;
         case 'image/jpeg' : imagejpeg($image, $cache_path.$cachename, $quality); break;
         default           : imagepng($image, $cache_path.$cachename);
     }
-    
+
     // Destruction
     @imagedestroy($image);
 }
 
 function jtpl_function_html_image_ombre ( $image, $array) {
-    
+
     // Default
     $leng = isset($array['soffset'])?$array['soffset']:10;
     $angle = isset($array['sangle'])?$array['sangle']:135;
     $flou = isset($array['sblur'])?$array['sblur']:10;
     $opac = isset($array['sopacity'])?$array['sopacity']:20;
     $color = isset($array['scolor'])?$array['scolor']:'#000000';
-    
+
     // Color of the shadow
     $color = str_replace('#', '', $color);
     $rgb = array(0,0,0);
@@ -253,10 +265,10 @@ function jtpl_function_html_image_ombre ( $image, $array) {
     else if (strlen($color) == 3)
         for ($x=0;$x<3;$x++)
             $rgb[$x] = hexdec(substr($color,(2*$x),1));
-    
+
     // Gaussian blur parameter
     $coeffs = array (array ( 1),
-                     array ( 1, 1), 
+                     array ( 1, 1),
                      array ( 1, 2, 1),
                      array ( 1, 3, 3, 1),
                      array ( 1, 4, 6, 4, 1),
@@ -269,14 +281,14 @@ function jtpl_function_html_image_ombre ( $image, $array) {
                      array ( 1, 11, 55, 165, 330, 462, 462, 330, 165, 55, 11, 1));
     $sum = pow (2, $flou);
     $demi = $flou/2;
-    
-    
+
+
     // Horizontal blur and blur margin
     $temp1 = imagecreatetruecolor(imagesx($image)+$flou, imagesy($image)+$flou);
     imagesavealpha($temp1, true);
     $tp = imagecolorallocatealpha($temp1,0,0,0,127);
     imagefill($temp1,0,0,$tp);
-    
+
     for ( $i=0 ; $i < imagesx($temp1) ; $i++ )
     for ( $j=0 ; $j < imagesy($temp1) ; $j++ ) {
         $ig = $i-$demi; $jg = $j-$demi; $suma = 0;
@@ -290,20 +302,20 @@ function jtpl_function_html_image_ombre ( $image, $array) {
         $c = imagecolorallocatealpha($temp1, 0, 0, 0, $suma/$sum );
         imagesetpixel($temp1,$i,$j,$c);
     }
-    
+
     // Vertical blur, a shift of the angle, opacity and color
-    
+
     $x = cos(deg2rad($angle))*$leng;
     $y = sin(deg2rad($angle))*$leng;
-    
+
     $temp2 = imagecreatetruecolor(imagesx($temp1)+abs($x), imagesy($temp1)+abs($y));
     imagesavealpha($temp2, true);
     $tp = imagecolorallocatealpha($temp2,0,0,0,127);
     imagefill($temp2,0,0,$tp);
-    
+
     $x1 = $x<0?0:$x;
     $y1 = $y<0?0:$y;
-    
+
     for ( $i=0 ; $i < imagesx($temp1) ; $i++ )
     for ( $j=0 ; $j < imagesy($temp1) ; $j++ ) {
         $suma = 0;
@@ -318,12 +330,12 @@ function jtpl_function_html_image_ombre ( $image, $array) {
         imagesetpixel($temp2,$i+$x1,$j+$y1,$c);
     }
     imagedestroy($temp1);
-    
+
     // Merge of the image and are shade
     $x = $x>0?0:$x;
     $y = $y>0?0:$y;
     imagecopy( $temp2, $image, $demi-$x, $demi-$y, 0, 0, imagesx($image), imagesy($image));
-    
+
     return $temp2;
 
 }
