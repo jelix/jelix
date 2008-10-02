13a8c52a03eb95415e865f4f3e9e291ca4ff1220
Dominique Papin <dominique.papin@ruedesecoles.com>
2008-10-02 07:47:08 +0000

    + update credits
    --HG--
    extra : convert_revision : svn%3Ab2e90908-df08-0410-9efa-d317cf624d33/trunk%401108

--- lib/jelix/plugins/tpl/html/block.form.php
+++ lib/jelix/plugins/tpl/html/block.form.php
@@ -3,9 +3,9 @@
 * @package     jelix
 * @subpackage  jtpl_plugin
 * @author      Jouanneau Laurent
-* @contributor Julien Issler, Bastien Jaillot
+* @contributor Julien Issler, Bastien Jaillot, Dominique Papin
 * @copyright   2006-2008 Jouanneau laurent
-* @copyright   2008 Julien Issler, 2008 Bastien Jaillot
+* @copyright   2008 Julien Issler, 2008 Bastien Jaillot, 2008 Dominique Papin
 * @link        http://www.jelix.org
 * @licence     GNU Lesser General Public Licence see LICENCE file or http://www.gnu.org/licenses/lgpl.html
 */
