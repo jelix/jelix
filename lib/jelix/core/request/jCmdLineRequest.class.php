dda2020405034e38123025af996ee415e205dce1
Laurent Jouanneau <laurent@jelix.org>
2008-12-11 10:08:18 +0000

    ticket #777: allow a CLI script to be dedicated to the default action, so we don't have to indicate the action as arguments of the script. trunk+1.1.x
    --HG--
    extra : convert_revision : svn%3Ab2e90908-df08-0410-9efa-d317cf624d33/trunk%401220

--- lib/jelix/core/request/jCmdLineRequest.class.php
+++ lib/jelix/core/request/jCmdLineRequest.class.php
@@ -24,6 +24,19 @@ class jCmdLineRequest extends jRequest {
 
     public $defaultResponseType = 'cmdline';
 
+    protected $onlyDefaultAction = false;
+    
+    /**
+     * If you want to have a CLI script dedicated to the default action,
+     * tell it by given true, so you haven't to indicate the action
+     * on the command line. It means of course you couldn't execute any other
+     * actions with this script.
+     * @param boolean $onlyDefaultAction 
+     */
+    function __construct($onlyDefaultAction = false){
+        $this->onlyDefaultAction = $onlyDefaultAction;
+    }
+
     public function isAllowedResponse($respclass){
         return ('jResponseCmdline' == $respclass);
     }
@@ -41,27 +54,37 @@ class jCmdLineRequest extends jRequest {
         $argv = $_SERVER['argv'];
         $scriptName = array_shift($argv); // shift the script name
 
-        // note: we cannot use jSelectorAct to parse the action
-        // because in the opt edition, jSelectorAct needs an initialized jCoordinator
-        // and this is not the case here. see bug #725.
-
-        if ($_SERVER['argc'] == 1) {
+        if ($this->onlyDefaultAction) {
             $mod = $gJConfig->startModule;
             $act = $gJConfig->startAction;
-        } else {
-            $argsel = array_shift($argv); // get the module~action selector
-            if ($argsel == 'help') {
+            if ($_SERVER['argc'] > 1 && $argv[0] == 'help') {
                 $mod = 'jelix';
                 $act = 'help:index';
-            }else if (($pos = strpos($argsel,'~')) !== false) {
-                $mod = substr($argsel,0,$pos);
-                $act = substr($argsel,$pos+1);
-            }else {
-                $mod= $gJConfig->startModule;
-                $act= $argsel;
+                $argv[0] = $gJConfig->startModule.'~'.$gJConfig->startAction;
             }
         }
-        
+        else {
+            // note: we cannot use jSelectorAct to parse the action
+            // because in the opt edition, jSelectorAct needs an initialized jCoordinator
+            // and this is not the case here. see bug #725.
+    
+            if ($_SERVER['argc'] == 1) {
+                $mod = $gJConfig->startModule;
+                $act = $gJConfig->startAction;
+            } else {
+                $argsel = array_shift($argv); // get the module~action selector
+                if ($argsel == 'help') {
+                    $mod = 'jelix';
+                    $act = 'help:index';
+                }else if (($pos = strpos($argsel,'~')) !== false) {
+                    $mod = substr($argsel,0,$pos);
+                    $act = substr($argsel,$pos+1);
+                }else {
+                    $mod= $gJConfig->startModule;
+                    $act= $argsel;
+                }
+            }
+        }
         $this->params = $argv;
         $this->params['module'] = $mod;
         $this->params['action'] = $act;
