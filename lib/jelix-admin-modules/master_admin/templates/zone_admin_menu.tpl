f8d74984faf00f25a6f4110db5ba93b57c1cf5db
Kévin Lepeltier <kevin@lepeltier.fr>
2009-01-22 10:42:10 +0000

    ticket #811: added the possibility to add any content in the menu of master_admin. p=lipki+me. trunk + 1.1.x
    --HG--
    extra : convert_revision : svn%3Ab2e90908-df08-0410-9efa-d317cf624d33/trunk%401288

--- lib/jelix-admin-modules/master_admin/templates/zone_admin_menu.tpl
+++ lib/jelix-admin-modules/master_admin/templates/zone_admin_menu.tpl
@@ -1,10 +1,14 @@
 {foreach $menuitems as $bloc}
-{if count($bloc->childItems)}
-<div class="menu-bloc" id="menu-bloc-{$bloc->id}">
-    {if $bloc->label}<h3>{$bloc->label|eschtml}</h3>{/if}
-    <ul>{foreach $bloc->childItems as $item}
-        <li{if $item->id == $selectedMenuItem} class="selected"{/if}{if $item->icon} style="background-image:url({$item->icon});"{/if}><a href="{$item->link|eschtml}">{$item->label|eschtml}</a></li>
-    {/foreach}</ul>
-</div>
-{/if}
+    {if count($bloc->childItems)}
+        <div class="menu-bloc" id="menu-bloc-{$bloc->id}">
+            {if $bloc->label}<h3>{$bloc->label|eschtml}</h3>{/if}
+            <ul>{foreach $bloc->childItems as $item}
+                <li {if $item->id == $selectedMenuItem} class="selected"{/if}
+                    {if $item->icon} style="background-image:url({$item->icon});"{/if}>
+                    {if $item->type == 'url'}<a href="{$item->content|eschtml}">{$item->label|eschtml}</a>
+                    {else}{$item->content}{/if}
+                </li>
+            {/foreach}</ul>
+        </div>
+    {/if}
 {/foreach}
--- lib/jelix-admin-modules/master_admin/zones/admin_menu.zone.php
+++ lib/jelix-admin-modules/master_admin/zones/admin_menu.zone.php
@@ -8,43 +8,12 @@
 * @licence  http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public Licence, see LICENCE file
 */
 
-class masterAdminMenuItem {
-    public $id = '';
-    public $parentId = '';
-    public $label = '';
-    public $link = '';
-    public $order = 0;
-    public $icon = '';
-    
-    public $childItems = array();
-    
-    public function __construct($id, $label, $link, $order=0, $parentId='') {
-        $this->id = $id;
-        $this->parentId = $parentId;
-        $this->label = $label;
-        $this->link = $link;
-        $this->order = $order;
-    }
-    
-    public function copyFrom($item) {
-        $this->label = $item->label;
-        $this->link = $item->link;
-        $this->order = $item->order;
-        $this->icon = $item->icon;
-        $this->childItems = array_merge($item->childItems, $this->childItems);
-    }
-}
-
-function masterAdminItemSort($itemA, $itemB)
-{
-    return ($itemA->order - $itemB->order);
-}
-
-
 class admin_menuZone extends jZone {
     protected $_tplname='zone_admin_menu';
 
     protected function _prepareTpl(){
+        jClasses::inc('masterAdminMenuItem');
+
         $menu = array();
         $menu['toplinks'] = new masterAdminMenuItem('toplinks', '', '');
         $menu['toplinks']->childItems[] = new masterAdminMenuItem('dashboard', jLocale::get('gui.menu.item.dashboard'), jUrl::get('default:index'));
