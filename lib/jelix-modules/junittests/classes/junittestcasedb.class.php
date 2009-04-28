71169923f18d7fc2a8288d58802833e211e1e481
Laurent Jouanneau <laurent@jelix.org>
2009-04-28 22:00:49 +0000

    junittest module: new method jUnitTestsDb::assertTableContainsRecordsByKeys
    --HG--
    extra : convert_revision : svn%3Ab2e90908-df08-0410-9efa-d317cf624d33/trunk%401409

--- lib/jelix-modules/junittests/classes/junittestcasedb.class.php
+++ lib/jelix-modules/junittests/classes/junittestcasedb.class.php
@@ -108,16 +108,16 @@ class jUnitTestCaseDb extends jUnitTestCase {
 
         $message = sprintf( $message, $table. " table should contains given records.");
 
-         $sql = 'SELECT * FROM '.$table;
-         $rs = $db->query($sql);
-         if(!$rs){
-            $this->fail($message.' ( no results set)');
-            return false;
-         }
-         $results = array();
-         foreach($rs as $r){
-            $results[]=get_object_vars($r);
-         }
+        $sql = 'SELECT * FROM '.$table;
+        $rs = $db->query($sql);
+        if(!$rs){
+           $this->fail($message.' ( no results set)');
+           return false;
+        }
+        $results = array();
+        foreach($rs as $r){
+           $results[]=get_object_vars($r);
+        }
 
         $globalok=true;
         $resultsSaved = $results;
@@ -157,6 +157,89 @@ class jUnitTestCaseDb extends jUnitTestCase {
             $this->dump($resultsSaved);
             $this->sendMessage('Records we should find');
             $this->dump($records);
+            return false;
+        }
+    }
+
+    /**
+     * check if all given record are in the table
+     * @param string $table the table name
+     * @param array $records the list of record we should find
+     * @param array|string $keys  the list of key names of records
+     * @param boolean $onlyThem  if true, check if the table has only this records
+     * @param string $message the error message
+     */
+    function assertTableContainsRecordsByKeys($table, $records, $keys, $onlyThem = true, $message ="%s"){
+        $db = jDb::getConnection($this->dbProfile);
+
+        if (is_string ($keys))
+            $keys = array($keys);
+
+        $message = sprintf( $message, $table. " table should contains given records.");
+
+        $sql = 'SELECT * FROM '.$table;
+        $rs = $db->query($sql);
+        if(!$rs){
+           $this->fail($message.' ( no results set)');
+           return false;
+        }
+        $results = array();
+        foreach($rs as $r){
+           $results[]=get_object_vars($r);
+        }
+
+        $globalok=true;
+        $resultsSaved = $results;
+        foreach($records as $rec) {
+            $found = false;
+            $res = array();
+            foreach($results as $k=>$res){
+                $keyok = true;
+                foreach ($keys as $keyname) {
+                    if($rec[$keyname] != $res[$keyname]) {
+                        $keyok = false;
+                        break;
+                    }
+                }
+                if ($keyok) {
+                    $found = true;
+                    break;
+                }
+            }
+
+            if(!$found){
+                $globalok = false;
+                $this->fail($message.'. No record found : '. var_export($rec,true));
+            }
+            else {
+                $sameValues = true;
+                foreach($rec as $name=>$value){
+                    if($res[$name] != $value) {
+                        $sameValues = false;
+                        break;
+                    }
+                }
+                unset($results[$k]);
+                if(!$sameValues){
+                    $globalok = false;
+                    $this->fail($message.'. Difference in a record. Actual:'. var_export($res,true). ' | Expected:'.var_export($rec,true));
+                    $this->diff(var_export($rec,true), var_export($res,true));
+                }
+            }
+        }
+
+        if($onlyThem && count($results) != 0){
+            $globalok = false;
+            $this->fail($message.'. Other unknow records exists');
+            $this->sendMessage('Unexpected records');
+            $this->dump($results);
+        }
+
+        if($globalok){
+            $this->pass($message);
+            return true;
+        }
+        else {
             return false;
         }
     }
