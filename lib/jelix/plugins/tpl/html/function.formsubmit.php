99a46ad103cb9a9655063a06b153be079cc39d46
Loic Mathaud <loic@mathaud.net>
2009-05-24 09:41:52 +0000

    fix bug #858 : there was no exception in formsubmit jTpl plugin if there was no submit control defined in the jForms file. r=laurentj
    --HG--
    extra : convert_revision : svn%3Ab2e90908-df08-0410-9efa-d317cf624d33/trunk%401421

--- lib/jelix/plugins/tpl/html/function.formsubmit.php
+++ lib/jelix/plugins/tpl/html/function.formsubmit.php
@@ -3,7 +3,8 @@
 * @package    jelix
 * @subpackage jtpl_plugin
 * @author     Laurent Jouanneau
-* @copyright  2007 Laurent Jouanneau
+* @contributor Loic Mathaud
+* @copyright  2007 Laurent Jouanneau, 2009 Loic Mathaud
 * @link        http://www.jelix.org
 * @licence    GNU Lesser General Public Licence see LICENCE file or http://www.gnu.org/licenses/lgpl.html
 */
@@ -22,14 +23,23 @@ function jtpl_function_html_formsubmit($tpl, $ctrlname='')
             $ctrl = $tpl->_privateVars['__submit'];
         }else{
             $ctrls = $tpl->_privateVars['__form']->getSubmits();
+            if (count($ctrls) == 0) {
+                throw new jException('jelix~formserr.unknow.control',
+                array('submit', $tpl->_privateVars['__form']->getSelector(),$tpl->_templateName));
+            }
             reset($ctrls);
             $ctrlname = key($ctrls);
             $ctrl = current($ctrls);
         }
     }else{
         $ctrls = $tpl->_privateVars['__form']->getSubmits();
+        if (count($ctrls) == 0) {
+            throw new jException('jelix~formserr.unknow.control',
+            array($ctrlname, $tpl->_privateVars['__form']->getSelector(),$tpl->_templateName));
+        }
         $ctrl = $ctrls[$ctrlname];
     }
+
     if($tpl->_privateVars['__form']->isActivated($ctrlname)) {
         $tpl->_privateVars['__displayed_submits'][$ctrlname] = true;
         $tpl->_privateVars['__formbuilder']->outputControl($ctrl);
