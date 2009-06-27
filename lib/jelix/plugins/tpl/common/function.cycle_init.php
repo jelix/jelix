61efe7602e7013ab134b6a1309e8c2fbd8ff6fcd
Laurent Jouanneau <laurent@jelix.org>
2009-06-27 13:24:21 +0000

    ticket #943: cycle_init should accept arrays as arguments
    --HG--
    extra : convert_revision : svn%3Ab2e90908-df08-0410-9efa-d317cf624d33/trunk%401437

--- lib/jelix/plugins/tpl/common/function.cycle_init.php
+++ lib/jelix/plugins/tpl/common/function.cycle_init.php
@@ -4,7 +4,7 @@
  * @subpackage  jtpl_plugin
  * @author      Philippe SCHELTE < dubphil >
  * @contributor Laurent Jouanneau
- * @copyright   2008 Philippe SCHELTE
+ * @copyright   2008 Philippe SCHELTE, 2009 Laurent Jouanneau
  * @link        http://jelix.org/
  * @licence     GNU Lesser General Public Licence see LICENCE file or http://www.gnu.org/licenses/lgpl.html
  */
@@ -32,18 +32,26 @@ function jtpl_function_common_cycle_init($tpl, $name, $values='') {
     if($name == ''){
         throw new jException("jelix~errors.tplplugin.cfunction.bad.argument.number", array('cycle_init','1',''));
     }
-    if(strpos($name,',') === false){
-        if($values == ''){
-            throw new jException("jelix~errors.tplplugin.cfunction.bad.argument.number", array('cycle_init','2',''));
-        }
-        if(strpos($values,',') === false){
-            throw new jException("jelix~errors.tplplugin.function.invalid", array('cycle_init','',''));
-        }
-    } else {
+    
+    if(is_array($name)) {
         $values = $name;
         $name = 'default';
     }
+    else if(strpos($name,',') === false){
+        if($values == ''){
+            throw new jException("jelix~errors.tplplugin.cfunction.bad.argument.number", array('cycle_init','2',''));
+        }
+        if (!is_array($values)) {
+            if(strpos($values,',') === false){
+                throw new jException("jelix~errors.tplplugin.function.invalid", array('cycle_init','',''));
+            }
+            $values = explode(',', $values);
+        }
+    } else {
+        $values = explode(',',$name);
+        $name = 'default';
+    }
 
-    $tpl->_privateVars['cycle'][$name]['values'] = explode(',',$values);
+    $tpl->_privateVars['cycle'][$name]['values'] = $values;
     $tpl->_privateVars['cycle'][$name]['index'] = 0;
 }
