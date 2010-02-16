0950678476bc09b8fd5ee499725004c399b4451a
Laurent Jouanneau <laurent@jelix.org>
2010-02-16 09:46:42 +0100

    updated php5redis to 1.1 version

--- lib/jelix/plugins/kvstore/redis/redis.kvdriver.php
+++ lib/jelix/plugins/kvstore/redis/redis.kvdriver.php
@@ -3,7 +3,8 @@
  * @package     jelix
  * @subpackage  kvstore
  * @author      Yannick Le Guédart
- * @copyright   2009 Yannick Le Guédart
+ * @contributor Laurent Jouanneau
+ * @copyright   2009 Yannick Le Guédart, 2010 Laurent Jouanneau
  *
  * @link     http://www.jelix.org
  * @licence  http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public Licence, see LICENCE file
@@ -11,94 +12,184 @@
 
 require_once(LIB_PATH . 'php5redis/Php5Redis.php');
 
-class redisKVDriver extends jKVDriver implements jIKVIncrementable, jIKVSet {
+class redisKVDriver extends jKVDriver implements jIKVSet, jIKVttl {
   
     /**
-	 * Connects to the redis server
-	 *
-	 * @return Php5Redis object
-	 */
+     * Connects to the redis server
+     *
+     * @return Php5Redis object
+     */
    	protected function _connect() {
 
         // A host is needed
         if (! isset($this->_profile['host'])) {
      		throw new jException(
-                'jelix~db.error.driver.nohost', $this->_driverName);           
+                'jelix~kvstore.error.no.host', $this->_profileName);
         }
  
         // A port is needed as well
         if (! isset($this->_profile['port'])) {
      		throw new jException(
-                'jelix~db.error.driver.noport', $this->_driverName);           
+                'jelix~kvstore.error.no.port', $this->_profileName);
         }
 
         // OK, let's connect now
         $cnx = new Php5Redis($this->_profile['host'], $this->_profile['port']);
         return $cnx;
-	}
+    }
 
     /**
-	 * Disconnect from the redis server
-	 *
-	 * @return Php5Redis object
-	 */
-   	protected function _disconnect() {
-		$this->_connection->quit();
+     * Disconnect from the redis server
+     */
+    protected function _disconnect() {
         $this->_connection->disconnect();
     }
 
-    protected function _doGet($key) {
-        return $this->_connection->get($key);
+    public function get($key) {
+        $res = $this->_connection->get($key);
+        if ($res === null)
+            return null;
+        $res = $this->unesc($res);
+        if (is_array($key)) {
+            return array_combine($key, $res);
+        }
+        else
+            return $res;
     }
 
-    protected function _doSet($key, $value, $ttl) {
-        $res = $this->_connection->set($key, $value);
+    public function set($key, $value) {
+        if (is_resource($value))
+            return false;
+        $res = $this->_connection->set($key, $this->esc($value));
+        return ($res === 'OK');
+    }
+
+    public function insert($key, $value) {
+        if (is_resource($value))
+            return false;
+        if ($this->_connection->exists($key) == 1)
+            return false;
+        $res = $this->_connection->set($key, $this->esc($value));
+        return ($res === 'OK');
+    }
+
+    public function replace($key, $value) {
+        if (is_resource($value))
+            return false;
+        if ($this->_connection->exists($key) == 0)
+            return false;
+        $res = $this->_connection->set($key, $this->esc($value));
+        return ($res === 'OK');
+    }
+
+    public function delete($key) {
+        return $this->_connection->delete($key);
+    }
+
+    public function flush() {
+        return $this->_connection->flushall();
+    }
+	
+    public function append($key, $value) {
+        if (is_resource($value))
+            return false;
+        $val = $this->_connection->get($key);
+        if ($val === null)
+            return false;
+        $val = $this->unesc($val).$value;
+        $res = $this->_connection->set($key, $this->esc($val));
+        if ($res !== 'OK')
+            return false;
+        else return $val;
+    }
+
+    public function prepend($key, $value) {
+        if (is_resource($value))
+            return false;
+        $val = $this->_connection->get($key);
+        if ($val === null)
+            return false;
+        $val = $value.$this->unesc($val);
+        $res = $this->_connection->set($key, $this->esc($val));
+        if ($res !== 'OK')
+            return false;
+        else return $val;
+    }
+
+    public function increment($key, $incvalue = 1) {
+        $val = $this->get($key);
+        if ($val === null || !is_numeric($val) || !is_numeric($incvalue))
+            return false;
+        return $this->_connection->incr($key, $incvalue);
+    }
+
+    public function decrement($key, $decvalue = 1) {
+        $val = $this->get($key);
+        if ($val === null || !is_numeric($val) || !is_numeric($decvalue))
+            return false;
+        return $this->_connection->decr($key, $decvalue);
+    }
+	
+    // jIKVttl -------------------------------------------------------------
+    public function setWithTtl($key, $value, $ttl) {
+        if (is_resource($value))
+            return false;
+
+        if ($ttl != 0 && $ttl > 2592000) {
+            $ttl -= time();
+        }
+        if ($ttl <= 0)
+            return true;
+
+        $res = $this->_connection->set($key, $this->esc($value));
 
         if ($res !== 'OK') {
             return false;
         }
 
-        if ($timeout != 0) {
-            return $this->_connection->expire($key, $ttl);
-        }
+        return $this->_connection->expire($key, $ttl);
+    }
 
+    public function garbage() {
         return true;
     }
 
-    protected function _doDelete($key) {
-        return $this->_connection->delete($key);
+    protected function esc($val) {
+        if (is_numeric($val) || is_int($val))
+            return (string)$val;
+        else 
+            return serialize($val);
     }
 
-    protected function _doFlush() {
-        return $this->_connection->flushall();
+    protected function unesc($val) {
+        if (is_numeric($val))
+            return floatval($val);
+        else if (is_string($val))
+            return unserialize($val);
+        else if (is_array($val)) {
+            foreach($val as $k=>$v) {
+                $val[$k] = $this->unesc($v);
+            }
+            return $val;
+        }
     }
 	
-	/* jIKVIncrementable ---------------------------------------------------- */
-	
-    public function increment($key, $incvalue = 1) {
-        return $this->_connection->incr($key, $incvalue);
-    }
+	// jIKVSet -------------------------------------------------------------
 
-    public function decrement($key, $decvalue = 1) {
-        return $this->_connection->decr($key, $decvalue);
-    }
-	
-	/* jIKVSet ------------------------------------------------------------- */
-	
-    public function set_add($skey, $value) {
+    public function sAdd($skey, $value) {
         return $this->_connection->sadd($skey, $value);
     }
 
-    public function set_remove($skey, $value) {
+    public function sRemove($skey, $value) {
         return $this->_connection->srem($skey, $decvalue);
     }
 
-    public function set_count($skey) {
+    public function sCount($skey) {
         return $this->_connection->scard($skey);
     }
-	
-	public function set_pop($skey) {
-		return $this->_connection->spop($skey);
-	}
+
+    public function sPop($skey) {
+        return $this->_connection->spop($skey);
+    }
 
 }
---
+++ testapp/modules/jelix_tests/tests/jkvstore.redis.html_cli.php
@@ -0,0 +1,36 @@
+<?php
+/**
+* @package     testapp
+* @subpackage  jelix_tests module
+* @author      Laurent Jouanneau
+* @copyright   2010 Laurent Jouanneau
+* @link        http://www.jelix.org
+* @licence     GNU Lesser General Public Licence see LICENCE file or http://www.gnu.org/licenses/lgpl.html
+*/
+
+require_once(dirname(__FILE__).'/jkvstore.lib.php');
+require_once(LIB_PATH . 'php5redis/Php5Redis.php');
+/**
+* Tests API jKVStore
+* @package     testapp
+* @subpackage  jelix_tests module
+*/
+
+class UTjKVStoreRedis extends UTjKVStore {
+
+    protected $profile = 'usingredis';
+
+    protected $redis;
+
+    public function setUp (){
+        $this->redis = new Php5Redis('localhost',6379);
+        $this->redis->flushall();
+    }
+
+    public function tearDown() {
+        //$this->redis->quit();
+        $this->redis->disconnect();
+    }
+}
+
+?>
