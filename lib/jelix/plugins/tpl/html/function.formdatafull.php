2bbfff977986f7ffa8c8265f67aba47a0ae2e6f0
dsdenes <dsdenes@gmail.com>
2010-03-22 10:12:15 +0100

    ticket #847: CRUD - formdatafull group colspan

--- lib/jelix/plugins/tpl/html/function.formdatafull.php
+++ lib/jelix/plugins/tpl/html/function.formdatafull.php
@@ -3,9 +3,9 @@
 * @package      jelix
 * @subpackage   jtpl_plugin
 * @author       Laurent Jouanneau
-* @contributor  Dominique Papin, Julien Issler, Brunto
+* @contributor  Dominique Papin, Julien Issler, Brunto, dsdenes
 * @copyright    2007-2010 Laurent Jouanneau, 2007 Dominique Papin
-* @copyright    2008 Julien Issler, 2010 Brunto
+* @copyright    2008 Julien Issler, 2010 Brunto, 2009 dsdenes
 * @link         http://www.jelix.org
 * @licence      GNU Lesser General Public Licence see LICENCE file or http://www.gnu.org/licenses/lgpl.html
 */
@@ -24,25 +24,28 @@ function jtpl_function_html_formdatafull($tpl, $form)
         if($ctrl->type == 'submit' || $ctrl->type == 'reset' || $ctrl->type == 'hidden' || $ctrl->type == 'captcha' || $ctrl->type == 'secretconfirm') continue;
         if(!$form->isActivated($ctrlref)) continue;
 
-        echo '<tr><th scope="row">';
+        echo '<tr><th scope="row"'.($ctrl->type == 'group' ? ' colspan="2" class="jforms-group"' : '').'>';
         echo htmlspecialchars($ctrl->label);
-        echo '</th><td>';
-        $value = $ctrl->getDisplayValue($form->getData($ctrlref));
-        if (is_array($value)) {
-            $s ='';
-            foreach ($value as $v) {
-                $s.=','.htmlspecialchars($v);
+        echo '</th>';
+        if ($ctrl->type != 'group') {
+            echo '<td>';
+            $value = $ctrl->getDisplayValue($form->getData($ctrlref));
+            if (is_array($value)) {
+                $s ='';
+                foreach ($value as $v) {
+                    $s.=','.htmlspecialchars($v);
+                }
+                echo substr($s, 1);
             }
-            echo substr($s, 1);
+            else if ($ctrl->isHtmlContent())
+                echo $value;
+            else if ($ctrl->type == 'textarea')
+                echo nl2br(htmlspecialchars($value));
+            else
+                echo htmlspecialchars($value);
+            echo '</td>';
         }
-        else if ($ctrl->isHtmlContent())
-            echo $value;
-        else if ($ctrl->type == 'textarea')
-            echo nl2br(htmlspecialchars($value));
-        else
-            echo htmlspecialchars($value);
-
-        echo '</td></tr>';
+        echo '</tr>';
     }
     echo '</table>';
 }
