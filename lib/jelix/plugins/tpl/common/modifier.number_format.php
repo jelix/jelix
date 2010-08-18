b53458af3642866dab339c29309f0873e4b2be44
Julien Issler <julien@issler.net>
2010-08-18 22:29:05 +0200

    ticket #1150: number_format doesn't like utf8 characters...

--- lib/jelix/plugins/tpl/common/modifier.number_format.php
+++ lib/jelix/plugins/tpl/common/modifier.number_format.php
@@ -5,7 +5,7 @@
  * @subpackage jtpl_plugin
  * @author     Julien Issler
  * @contributor Mickael Fradin
- * @copyright  2008 Julien Issler, 2009 Mickael Fradin
+ * @copyright  2008-2010 Julien Issler, 2009 Mickael Fradin
  * @link       http://www.jelix.org
  * @licence    GNU Lesser General Public Licence see LICENCE file or http://www.gnu.org/licenses/lgpl.html
  * @since 1.1
@@ -26,6 +26,13 @@ function jtpl_modifier_common_number_format($number, $decimals=0, $dec_point=fal
     }
     if ($thousands_sep === false) {
         $thousands_sep = jLocale::get('jelix~format.thousands_sep');
+        if(strlen($thousands_sep) > 1){
+            $real_thousands_sep = $thousands_sep;
+            $thousands_sep = '#';
+        }
     }
-    return number_format($number, $decimals, $dec_point, $thousands_sep);
+    $number = number_format($number, $decimals, $dec_point, $thousands_sep);
+    if(isset($real_thousands_sep))
+        return str_replace($thousands_sep, $real_thousands_sep, $number);
+    return $number;
 }
