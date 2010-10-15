2fe5ff0f8158c675ae9b1773210592171b46c8d2
Laurent Jouanneau <laurent@jelix.org>
2010-10-15 18:11:20 +0200

    jTpl plugin: parameters for formurlparam are now optional and values are retrieved from formurl parameters
    --HG--
    extra : rebase_source : 4c9304407a1656e1cca71045939384ec67fedd0e

--- lib/jelix/plugins/tpl/html/function.formurl.php
+++ lib/jelix/plugins/tpl/html/function.formurl.php
@@ -4,7 +4,7 @@
 * @subpackage jtpl_plugin
 * @author     Loic Mathaud
 * @contributor Laurent Jouanneau
-* @copyright  2005-2006 Loic Mathaud, 2008 Laurent Jouanneau
+* @copyright  2005-2006 Loic Mathaud, 2008-2010 Laurent Jouanneau
 * @link        http://www.jelix.org
 * @licence    GNU Lesser General Public Licence see LICENCE file or http://www.gnu.org/licenses/lgpl.html
 */
@@ -20,5 +20,8 @@ function jtpl_function_html_formurl($tpl, $selector, $params=array())
 {
     $url = jUrl::get($selector, $params, 2); // retourne le jurl correspondant
     echo $url->getPath();
+    
+    $tpl->_privateVars['_formurl'] = $url;
+    
 }
 
