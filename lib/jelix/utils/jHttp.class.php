10786d8c5bfb7b21c6ae2b99cbbcd9caba08db71
Laurent Jouanneau <laurent@jelix.org>
2010-10-24 00:26:04 +0200

    added put and delete method on jHttp

--- lib/jelix/utils/jHttp.class.php
+++ lib/jelix/utils/jHttp.class.php
@@ -4,7 +4,7 @@
 * @subpackage  utils
 * @author      Laurent Jouanneau
 * @contributor 
-* @copyright   2008 Laurent Jouanneau
+* @copyright   2008-2010 Laurent Jouanneau
 * @link        http://www.jelix.org
 * @licence     GNU Lesser General Public Licence see LICENCE file or http://www.gnu.org/licenses/lgpl.html
 */
@@ -20,6 +20,52 @@ require(LIB_PATH.'clearbricks/net.http/class.net.http.php');
  */
 class jHttp extends netHttp {
     protected $user_agent = 'Clearbricks/Jelix HTTP Client';
+
+
+	/**
+	* DELETE Request
+	*
+	* Executes a DELETE request for the specified path. If <var>$data</var> is
+	* specified, appends it to a query string as part of the get request.
+	* <var>$data</var> can be an array of key value pairs, in which case a
+	* matching query string will be constructed. Returns true on success.
+	* 
+	* @param string	$path			Request path
+	* @param array		$data			Request parameters
+	* @return boolean
+	*/
+	public function delete($path,$data=false)
+	{
+		$this->path = $path;
+		$this->method = 'DELETE';
+		
+		if ($data) {
+			$this->path .= '?'.$this->buildQueryString($data);
+		}
+		
+		return $this->doRequest();
+	}
+	
+	/**
+	* PUT Request
+	*
+	* Executes a PUT request for the specified path.
+	*
+	* @param string	$path			Request path
+	* @param array		$data			Request parameters
+	* @param array		$charset			Request charset
+	* @return boolean
+	*/
+	public function put($path,$data,$charset=null)
+	{
+		if ($charset) {
+			$this->post_charset = $charset;
+		}
+		$this->path = $path;
+		$this->method = 'PUT';
+		$this->postdata = $this->buildQueryString($data);
+		return $this->doRequest();
+	}
 
     protected function debug($msg,$object=false){
         if ($this->debug) {
