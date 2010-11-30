bbc416146a7a60a40722a48ec61fbf00373bcb0e
Rob2 <rob2.no-mail@jelix.org>
2010-11-30 23:53:46 +0100

    ticket #1240: image plugin: alt attribute is not generated when it is empty. trunk+1.1.x

--- lib/jelix/plugins/tpl/html/function.image.php
+++ lib/jelix/plugins/tpl/html/function.image.php
@@ -3,8 +3,8 @@
  * @package     jelix
  * @subpackage  jtpl_plugin
  * @author      Lepeltier kévin
- * @contributor Dominique Papin
- * @copyright   2007-2008 Lepeltier kévin, 2008 Dominique Papin
+ * @contributor Dominique Papin, Rob2
+ * @copyright   2007-2008 Lepeltier kévin, 2008 Dominique Papin, 2010 Rob2
  * @link        http://www.jelix.org
  * @licence     GNU Lesser General Public Licence see LICENCE file or http://www.gnu.org/licenses/lgpl.html
  */
@@ -60,7 +60,7 @@ function jtpl_function_html_image($tpl, $src, $params=array()) {
     // generating hmtl tag img
     echo '<img';
     foreach( $att as $key => $val ) {
-        if( !empty($val) )
+        if( !empty($val) || $key == 'alt' )
             echo ' '.$key.'="'.htmlspecialchars($val).'"';
     }
     echo '/>';
