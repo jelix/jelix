87049fd9840824696f4721890c2bbe8db9c05feb
Laurent Jouanneau <laurent@jelix.org>
2011-01-13 17:40:31 +0100

    Ticket #1259: sql error in install script for sqlite, in jacl2db module

--- lib/jelix-modules/jacl2db/install/install_jacl2.schema.sqlite.sql
+++ lib/jelix-modules/jacl2db/install/install_jacl2.schema.sqlite.sql
@@ -1,15 +1,15 @@
 
 
-CREATE TABLE hf_jacl2_subject (
+CREATE TABLE %%PREFIX%%%jacl2_subject (
   id_aclsbj varchar(100) NOT NULL DEFAULT '',
   label_key varchar(100) DEFAULT NULL,
   PRIMARY KEY (id_aclsbj)
 ) ;
 
 
-CREATE TABLE hf_jacl2_user_group (
+CREATE TABLE %%PREFIX%%%jacl2_user_group (
   login varchar(50) NOT NULL DEFAULT '',
   id_aclgrp int(11) NOT NULL DEFAULT '0'
 ) ;
-CREATE UNIQUE INDEX hf_jacl2_user_group_login ON hf_jacl2_user_group (login);
+CREATE UNIQUE INDEX %%PREFIX%%%jacl2_user_group_login ON %%PREFIX%%%jacl2_user_group (login);
 
