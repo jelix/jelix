b330524c8855fef1eb02f0618a8c236f69b9cfe4
René-Luc D'Hont <rldhont@gmail.com>
2011-02-22 11:10:03 +0100

    Ticket #1279 - added missing table in install_jacl2.schema.sqlite.sql

--- lib/jelix-modules/jacl2db/install/install_jacl2.schema.sqlite.sql
+++ lib/jelix-modules/jacl2db/install/install_jacl2.schema.sqlite.sql
@@ -1,4 +1,11 @@
 
+CREATE TABLE %%PREFIX%%jacl2_group (
+    id_aclgrp AUTOINCREMENT PRIMARY KEY,
+    name varchar(150) NOT NULL DEFAULT '',
+    code varchar(30),
+    grouptype int(5) NOT NULL DEFAULT '0',
+    ownerlogin varchar(50)
+);
 
 CREATE TABLE %%PREFIX%%jacl2_subject (
   id_aclsbj varchar(100) NOT NULL DEFAULT '',
@@ -6,10 +13,16 @@ CREATE TABLE %%PREFIX%%jacl2_subject (
   PRIMARY KEY (id_aclsbj)
 ) ;
 
-
 CREATE TABLE %%PREFIX%%jacl2_user_group (
   login varchar(50) NOT NULL DEFAULT '',
-  id_aclgrp int(11) NOT NULL DEFAULT '0'
+  id_aclgrp int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (login, id_aclgrp)
 ) ;
-CREATE UNIQUE INDEX %%PREFIX%%jacl2_user_group_login ON %%PREFIX%%jacl2_user_group (login);
+
+CREATE TABLE %%PREFIX%%jacl2_rights (
+  id_aclsbj varchar(100) NOT NULL DEFAULT '',
+  id_aclgrp int(11) NOT NULL DEFAULT '0',
+  id_aclres varchar(100) NOT NULL DEFAULT '',
+  PRIMARY KEY (id_aclsbj,id_aclgrp,id_aclres)
+) ;
 
