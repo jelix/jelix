d091115f9914d1b74908ef5a3321c227d31ba939
Julien Issler <julien@issler.net>
2011-03-07 19:11:46 +0100

    fixed syntax error introduced in changeset 3c15cbe7e974

--- lib/jelix-modules/jauth/install/install.php
+++ lib/jelix-modules/jauth/install/install.php
@@ -3,8 +3,9 @@
 * @package     jelix
 * @subpackage  jauth module
 * @author      Laurent Jouanneau
-* @contributor
+* @contributor Julien Issler
 * @copyright   2009-2010 Laurent Jouanneau
+* @copyright   2011 Julien Issler
 * @link        http://www.jelix.org
 * @licence     GNU Lesser General Public Licence see LICENCE file or http://www.gnu.org/licenses/lgpl.html
 */
@@ -31,7 +32,7 @@ class jauthModuleInstaller extends jInstallerModule {
             if ($this->firstExec('auth:'.$authconfig)) {
                 // no configuration, let's install the plugin for the entry point
                 $this->config->setValue('auth', $authconfig, 'coordplugins');
-                if (!file_exists(jApp::configPath($authconfig)) {
+                if (!file_exists(jApp::configPath($authconfig))) {
                     $this->copyFile('var/config/'.$pluginIni, jApp::configPath($authconfig));
                 }
             }
