4131e48b775c06b49a4f70ab1b3f7c864a6c8681
Julien Issler <julien@issler.net>
2011-08-01 10:51:48 +0200

    ticket #1340 - jTpl: formdatafull should output a list for multiple-items controls

--- lib/jelix/plugins/tpl/html/function.formdatafull.php
+++ lib/jelix/plugins/tpl/html/function.formdatafull.php
@@ -5,7 +5,7 @@
 * @author       Laurent Jouanneau
 * @contributor  Dominique Papin, Julien Issler, Brunto, DSDenes
 * @copyright    2007-2010 Laurent Jouanneau, 2007 Dominique Papin
-* @copyright    2008 Julien Issler, 2010 Brunto, 2009 DSDenes
+* @copyright    2008-2010 Julien Issler, 2010 Brunto, 2009 DSDenes
 * @link         http://www.jelix.org
 * @licence      GNU Lesser General Public Licence see LICENCE file or http://www.gnu.org/licenses/lgpl.html
 */
@@ -30,12 +30,11 @@ function jtpl_function_html_formdatafull($tpl, $form)
         if ($ctrl->type != 'group') {
             echo '<td>';
             $value = $ctrl->getDisplayValue($form->getData($ctrlref));
-            if (is_array($value)) {
-                $s ='';
-                foreach ($value as $v) {
-                    $s.=','.htmlspecialchars($v);
-                }
-                echo substr($s, 1);
+            if (is_array($value) && !empty($value)) {
+                echo '<ul>';
+                foreach ($value as $v)
+                    echo '<li>',nl2br(htmlspecialchars($v)),'</li>';
+                echo '</ul>';
             }
             else if ($ctrl->isHtmlContent())
                 echo $value;
