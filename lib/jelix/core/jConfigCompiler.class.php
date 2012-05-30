fb56dc5cce8f457105ea180638ee467a639cca37
Laurent Jouanneau <laurent@jelix.org>
2012-05-30 19:46:07 +0200

    changed the notation in configuration to indicate a response class of a module. Same syntax as selectors. ex: html=mymodule~myresponse. the prefix 'module:' is optional

--- lib/jelix/core/jConfigCompiler.class.php
+++ lib/jelix/core/jConfigCompiler.class.php
@@ -613,30 +613,32 @@ class jConfigCompiler {
      */
     static private function _initResponsesPath($config, $list){
         $copylist = $config->$list; // because we modify $list and then it will search for "foo.path" responses...
-        foreach($copylist as $type=>$class){
+        foreach ($copylist as $type=>$class) {
             if (strpos($class,'app:') === 0) {
                 $config->{$list}[$type] = $class = substr($class, 4);
                 $config->{$list}[$type.'.path'] = $path = jApp::appPath('responses/'.$class.'.class.php');
                 if (file_exists($path))
                     continue;
             }
-            else if (preg_match('@^module:([^:]+)(\:(.+))?$@', $class, $m)) {
+            else if (preg_match('@^(?:module:)?([^~]+)~(.+)$@', $class, $m)) {
                 $mod = $m[1];
-                if (isset($config->_modulesPathList[$mod]) && isset($m[2])) {
-                    $class = $m[3];
+                if (isset($config->_modulesPathList[$mod])) {
+                    $class = $m[2];
                     $path = $config->_modulesPathList[$mod].'responses/'.$class.'.class.php';
                     $config->{$list}[$type] = $class;
                     $config->{$list}[$type.'.path'] = $path;
                     if (file_exists($path))
                         continue;
                 }
+                else
+                    $path = $class;
             }
-            else if(file_exists($path=JELIX_LIB_CORE_PATH.'response/'.$class.'.class.php')){
-                $config->{$list}[$type.'.path']=$path;
+            else if (file_exists($path=JELIX_LIB_CORE_PATH.'response/'.$class.'.class.php')) {
+                $config->{$list}[$type.'.path'] = $path;
                 continue;
             }
-            else if(file_exists($path=jApp::appPath('responses/'.$class.'.class.php'))){
-                $config->{$list}[$type.'.path']=$path;
+            else if (file_exists($path=jApp::appPath('responses/'.$class.'.class.php'))) {
+                $config->{$list}[$type.'.path'] = $path;
                 continue;
             }
             throw new Exception('Error in main configuration on responses parameters -- the class file of the response type "'.$type.'" is not found ('.$path.')',12);
