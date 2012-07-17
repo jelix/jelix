jelix/jelix

commit 7999de8605bd4bd77fa51049c4610b74c7c602cb

The plugins directories of modules are now automatically registered

lib/jelix/core/jConfigCompiler.class.php
@@ -378,6 +378,9 @@ class jConfigCompiler {
                         elseif ($config->modules[$f.'.access'])
                             $config->_modulesPathList[$f]=$p.$f.'/';
                             self::readModuleFile($config, $p.$f.'/');
+                            if (file_exists( $p.$f.'/plugins')) {
+                                $config->pluginsPath .= ',module:'.$f;
+                            }
                     }
                 }
                 closedir($handle);

commit fe30028f77df52d73f551cf4ae21e2ef424f76ae

Commit 1881989619 did fixed an issue introduced by commit 7999de8605, but previous code was poorly indented and thus it leaded to mis-reading. I did have a problem loading testapp's PHP tests which leaded to a crash : PHP Fatal error:  Class 'jAclDbUserGroup' not found in ...testapp/modules/jelix_tests/tests/jacl.main_api.html_cli.php . I do not understand clearly the problem, but this solves the issue and seems to be keeping the behaviour before commit 7999de8605 while preserving its enhancements

lib/jelix/core/jConfigCompiler.class.php
@@ -377,11 +377,11 @@ class jConfigCompiler {
                         }
                         elseif ($config->modules[$f.'.access']) {
                             $config->_modulesPathList[$f]=$p.$f.'/';
-                            self::readModuleFile($config, $p.$f.'/');
                             if (file_exists( $p.$f.'/plugins')) {
                                 $config->pluginsPath .= ',module:'.$f;
                             }
                         }
+                        self::readModuleFile($config, $p.$f.'/');
                     }
                 }
                 closedir($handle);