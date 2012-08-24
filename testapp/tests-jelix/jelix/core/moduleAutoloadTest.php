9eef4e8f419e86e4aa5f8fc864a849cf1c3bf7f9
Laurent Jouanneau <ljouanneau@gmail.com>
2012-08-25 00:15:58 +0200

    new soap module - fixed a test

--- testapp/tests-jelix/jelix/core/moduleAutoloadTest.php
+++ testapp/tests-jelix/jelix/core/moduleAutoloadTest.php
@@ -24,7 +24,7 @@ class moduleAutoloadTest extends PHPUnit_Framework_TestCase
     
     function testClassSection() {
         $conf = jApp::config();
-        $this->assertEquals(2, count($conf->_autoload_class), '_autoload_class should have 2 declarations (for jelix_tests and jacl modules)');
+        $this->assertEquals(3, count($conf->_autoload_class), '_autoload_class should have 2 declarations (for jelix_tests, jsoap and jacl modules)');
         $this->assertTrue(isset($conf->_autoload_class['myautoloadedclass']), '_autoload_class should declare info for myautoloadedclass');
         $this->assertEquals(self::$modulePath.'autoloadtest/autoloadtestclass.php', $conf->_autoload_class['myautoloadedclass'] , 'check path of file for myautoloadedclass');
     }
