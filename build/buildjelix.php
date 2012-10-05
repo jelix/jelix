7cd4dd0a2e78215ce9e22dfa9adc7d7f63ecbfbc
Philippe Villiers <kissifrot@gmail.com>
2012-10-05 18:43:30 +0200

    Fix verbose mode not working with -v switch.
    Conflicts:
    
    	build/buildjelix.php
    	build/buildmodules.php

--- build/buildjelix.php
+++ build/buildjelix.php
@@ -151,7 +151,7 @@ $BUILD_OPTIONS = array(
     false,
     '',
     ),
-'VERBOSE'=> array(
+'VERBOSE_MODE'=> array(
     "show messages",
     false,
     ),
@@ -281,7 +281,7 @@ if ($TARGET_REPOSITORY != '') {
 jBuildUtils::createDir($BUILD_TARGET_PATH);
 
 jManifest::$stripComment = ($STRIP_COMMENT == '1');
-jManifest::$verbose = ($VERBOSE == '1');
+jManifest::$verbose = ($VERBOSE_MODE == '1');
 jManifest::setFileSystem($TARGET_REPOSITORY);
 jManifest::$sourcePropertiesFilesDefaultCharset = $DEFAULT_CHARSET;
 jManifest::$targetPropertiesFilesCharset = $PROPERTIES_CHARSET_TARGET;
