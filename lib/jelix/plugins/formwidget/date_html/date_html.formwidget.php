b52b51573375e4be4c220b2b3eef93c7bd5ee86c
Laurent Jouanneau <laurent@jelix.org>
2012-11-13 15:22:05 +0100

    jForms plugins: fixed copyrights

--- lib/jelix/plugins/formwidget/date_html/date_html.formwidget.php
+++ lib/jelix/plugins/formwidget/date_html/date_html.formwidget.php
@@ -1,9 +1,11 @@
 <?php
 /**
 * @package     jelix
-* @subpackage  forms
+* @subpackage  formwidgets
 * @author      Claudio Bernardes
+* @contributor Laurent Jouanneau, Julien Issler, Dominique Papin
 * @copyright   2012 Claudio Bernardes
+* @copyright   2006-2012 Laurent Jouanneau, 2008-2011 Julien Issler, 2008 Dominique Papin
 * @link        http://www.jelix.org
 * @licence     http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public Licence, see LICENCE file
 */
