25580b79bc948afd7050eee4dfb56486e4a838ac
Laurent Jouanneau <ljouanneau@gmail.com>
2012-11-26 22:15:31 +0100

    Unit tests: clean the temp directory before tests

--- testapp/tests-jelix/bootstrap.php
+++ testapp/tests-jelix/bootstrap.php
@@ -5,6 +5,7 @@ require_once(LIB_PATH.'jelix-tests/classes/junittestcase.class.php');
 require_once(LIB_PATH.'jelix-tests/classes/junittestcasedb.class.php');
 
 jApp::setEnv('jelixtests');
+jAppManager::clearTemp(jApp::tempPath());
 
 define('TESTAPP_URL', 'http://testapp.local/');
 define('TESTAPP_HOST', 'testapp.local');
