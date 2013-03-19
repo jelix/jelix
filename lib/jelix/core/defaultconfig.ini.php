ec5f76c70477ba73bbc3707e381fb150ec7544be
Laurent Jouanneau <ljouanneau@gmail.com>
2013-03-19 09:43:19 +0100

    Marked loadClasses config parameter as deprecated
    The autoload configuration in module.xml file should be used instead.
    ticket #1508

--- lib/jelix/core/defaultconfig.ini.php
+++ lib/jelix/core/defaultconfig.ini.php
@@ -352,6 +352,7 @@ storage=
 ; dao_db_profile = ""
 
 ; list of selectors of classes to load before the session_start
+; @deprecated please use autoload configuration in module.xml files instead
 loadClasses=
 
 [forms]
