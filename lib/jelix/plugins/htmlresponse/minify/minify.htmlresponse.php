jelix/jelix

commit 1b18453de27f89b85aba9904a488024799832270

Ticket #1461 - minify, bad exlusion of files
we should take care of the base path in indicated url

lib/jelix/plugins/htmlresponse/minify/minify.htmlresponse.php
@@ -38,9 +38,14 @@ class minifyHTMLResponsePlugin implements jIHTMLResponsePlugin {
         if (!($this->response instanceof jResponseHtml))
             return;
         global $gJConfig;
+        $basePath = $gJConfig->urlengine['basePath'];
         if ($gJConfig->jResponseHtml['minifyCSS']) {
             if ($gJConfig->jResponseHtml['minifyExcludeCSS']) {
-                $this->excludeCSS = explode( ',', $gJConfig->jResponseHtml['minifyExcludeCSS'] );
+                $this->excludeCSS = preg_split( '!\s*/\s*!', $gJConfig->jResponseHtml['minifyExcludeCSS'] );
+                foreach($this->excludeCSS as $k=>$url) {
+                    if (substr($url,0,1) != '/')
+                        $this->excludeCSS[$k]= $basePath.$url;
+                }
             }
 
             $this->response->setCSSLinks($this->generateMinifyList($this->response->getCSSLinks(), 'excludeCSS'));
@@ -49,7 +54,11 @@ class minifyHTMLResponsePlugin implements jIHTMLResponsePlugin {
 
         if ($gJConfig->jResponseHtml['minifyJS']) {
             if($gJConfig->jResponseHtml['minifyExcludeJS'] ) {
-                $this->excludeJS = explode( ',', $gJConfig->jResponseHtml['minifyExcludeJS'] );
+                $this->excludeJS = preg_split( '!\s*/\s*!', $gJConfig->jResponseHtml['minifyExcludeJS'] );
+                foreach($this->excludeJS as $k=>$url) {
+                    if (substr($url,0,1) != '/')
+                        $this->excludeJS[$k]= $basePath.$url;
+                }
             }
             $this->response->setJSLinks($this->generateMinifyList($this->response->getJSLinks(), 'excludeJS'));
             $this->response->setJSIELinks($this->generateMinifyList($this->response->getJSIELinks(), 'excludeJS'));

lib/jelix/core/defaultconfig.ini.php
@@ -122,7 +122,7 @@ minifyCSS = off
 minifyJS = off
 ; list of filenames which shouldn't be minified. Path relative to basePath:
 minifyExcludeCSS = ""
-minifyExcludeJS = "jquery.wymeditor.js"
+minifyExcludeJS = "jelix/wymeditor/jquery.wymeditor.js"
 
 [debugbar]
 plugins = sqllog,sessiondata,defaultlog

commit f5b8304d8b44a6659a092466eac5fe2959784f53

Minify plugin: fixed a regression
introduced by commit 1b18453de27f89b85aba9904a488024799832270.
the list of excluded CSS or JS files was splitted on /, not on a comma.
bad copy-paste /o\

lib/jelix/plugins/htmlresponse/minify/minify.htmlresponse.php
@@ -41,7 +41,7 @@ class minifyHTMLResponsePlugin implements jIHTMLResponsePlugin {
         $basePath = $gJConfig->urlengine['basePath'];
         if ($gJConfig->jResponseHtml['minifyCSS']) {
             if ($gJConfig->jResponseHtml['minifyExcludeCSS']) {
-                $this->excludeCSS = preg_split( '!\s*/\s*!', $gJConfig->jResponseHtml['minifyExcludeCSS'] );
+                $this->excludeCSS = preg_split( '/\s*,\s*/', $gJConfig->jResponseHtml['minifyExcludeCSS'] );
                 foreach($this->excludeCSS as $k=>$url) {
                     if (substr($url,0,1) != '/')
                         $this->excludeCSS[$k]= $basePath.$url;
@@ -54,7 +54,7 @@ class minifyHTMLResponsePlugin implements jIHTMLResponsePlugin {
 
         if ($gJConfig->jResponseHtml['minifyJS']) {
             if($gJConfig->jResponseHtml['minifyExcludeJS'] ) {
-                $this->excludeJS = preg_split( '!\s*/\s*!', $gJConfig->jResponseHtml['minifyExcludeJS'] );
+                $this->excludeJS = preg_split( '/\s*,\s*/', $gJConfig->jResponseHtml['minifyExcludeJS'] );
                 foreach($this->excludeJS as $k=>$url) {
                     if (substr($url,0,1) != '/')
                         $this->excludeJS[$k]= $basePath.$url;