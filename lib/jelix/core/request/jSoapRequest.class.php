fc638ca8185568432593a6e526936bc0cb1f91b1
Laurent Jouanneau <ljouanneau@gmail.com>
2013-09-18 16:43:40 +0200

    Fixed indentation

--- lib/jelix/core/request/jSoapRequest.class.php
+++ lib/jelix/core/request/jSoapRequest.class.php
@@ -67,27 +67,29 @@ class jSoapRequest extends jRequest {
     * @param boolean $useDefaultIfEmpty true: says to return the default value if the parameter value is ""
     * @return mixed the request parameter value
     */
-    
     public function getParam($name, $defaultValue=null, $useDefaultIfEmpty=false){
-    	if(isset($this->params[$name])){
-			// we cannot use the empty() function because 0 returns true. And maybe we want 0 as a normal value...
-			if(is_scalar($this->params[$name])){
-				if( $useDefaultIfEmpty && trim($this->params[$name]) == '' ){
-					return $defaultValue;
-				}else{
-					return $this->params[$name];
-				}
-			}else{
-				// array or object
-				if( $useDefaultIfEmpty && empty($this->params[$name]) ){
-					return $defaultValue;
-				}else{
-					return $this->params[$name];
-				}
-			}
-		}else{
-			return $defaultValue;
-		}
+        if (!isset($this->params[$name])) {
+            return $defaultValue;
+        }
+        // we cannot use the empty() function because 0 returns true. And maybe we want 0
+        // as a normal value...
+        if (is_scalar($this->params[$name])) {
+            if ($useDefaultIfEmpty && trim($this->params[$name]) == '' ) {
+                return $defaultValue;
+            }
+            else {
+                return $this->params[$name];
+            }
+        }
+        else{
+            // array or object
+            if ( $useDefaultIfEmpty && empty($this->params[$name]) ) {
+                return $defaultValue;
+            }
+            else {
+                return $this->params[$name];
+            }
+        }
     }
 
 }
