3353fc585b46c7204d4aef71c8475c098445aaa9
Laurent Jouanneau <ljouanneau@gmail.com>
2014-02-19 22:13:46 +0100

    lib/jelix/ renaming: fixed the autoloader.

--- lib/jelix-legacy/init.php
+++ lib/jelix-legacy/init.php
@@ -131,7 +131,7 @@ require (JELIX_LIB_CORE_PATH . 'jSession.class.php');
  * @name $gLibPath
  * @see __autoload()
  */
-$gLibPath=array('Db'=>JELIX_LIB_PATH.'db/', 'Dao'=>JELIX_LIB_PATH.'dao/',
+$GLOBALS['gLibPath'] =array('Db'=>JELIX_LIB_PATH.'db/', 'Dao'=>JELIX_LIB_PATH.'dao/',
  'Forms'=>JELIX_LIB_PATH.'forms/', 'Event'=>JELIX_LIB_PATH.'events/',
  'Tpl'=>JELIX_LIB_PATH.'tpl/', 'Controller'=>JELIX_LIB_PATH.'controllers/',
  'Auth'=>JELIX_LIB_PATH.'auth/', 'Installer'=>JELIX_LIB_PATH.'installer/',
@@ -142,7 +142,7 @@ $gLibPath=array('Db'=>JELIX_LIB_PATH.'db/', 'Dao'=>JELIX_LIB_PATH.'dao/',
  */
 function jelix_autoload($class) {
     if (strpos($class, 'jelix\\') === 0) {
-        $f = LIB_PATH.str_replace('\\', DIRECTORY_SEPARATOR, $class).'.php';
+        $f = LIB_PATH.'jelix-legacy/'.str_replace('\\', DIRECTORY_SEPARATOR, substr($class,6)).'.php';
     }
     else if(preg_match('/^j(Dao|Tpl|Event|Db|Controller|Forms|Auth|Installer|KV).*/i', $class, $m)){
         $f=$GLOBALS['gLibPath'][$m[1]].$class.'.class.php';
