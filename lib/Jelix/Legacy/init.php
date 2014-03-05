b5f2fe1e2a0a3e633b07b13017c0b6634ffdb006
Laurent Jouanneau <ljouanneau@gmail.com>
2014-03-05 11:04:57 +0100

    Moved legacy classes mapping to a json file

--- lib/Jelix/Legacy/init.php
+++ lib/Jelix/Legacy/init.php
@@ -8,25 +8,11 @@
 * @licence  MIT
 */
 
-$GLOBALS['JELIX_LEGACY_CLASSES'] = array(
-    "jApp" => __DIR__.'/core/jApp.php',
-    "jAppManager" => __DIR__.'/core/jAppManager.php',
-    "jAutoloader" => __DIR__.'/core/jAutoloader.php',
-    "jClasses" => __DIR__.'/utils/jClasses.php',
-    "jClassBinding" => __DIR__.'/utils/jClassBinding.php',
-    "jConfig" => __DIR__.'/core/jConfig.php',
-    "jConfigAutoloader" => __DIR__.'/core/jConfigAutoloader.php',
-    "jConfigCompiler" => __DIR__.'/core/jConfigCompiler.php',
-    "jIMultiFileCompiler" => __DIR__.'/core/jIMultiFileCompiler.php',
-    "jIncluder" => __DIR__.'/core/jIncluder.php',
-    "jISimpleCompiler" => __DIR__.'/core/jISimpleCompiler.php',
-    "jProfiles" => __DIR__.'/utils/jProfiles.php',
-);
-
+$GLOBALS['JELIX_LEGACY_CLASSES'] = json_decode(file_get_contents(__DIR__.'/mapping.json'), true);
 
 function jelix_legacy_autoload($class) {
     if (isset($GLOBALS['JELIX_LEGACY_CLASSES'][$class])) {
-        $f = $GLOBALS['JELIX_LEGACY_CLASSES'][$class];
+        $f = __DIR__.'/'.$GLOBALS['JELIX_LEGACY_CLASSES'][$class];
         if (file_exists($f)) {
             require($f);
         }
