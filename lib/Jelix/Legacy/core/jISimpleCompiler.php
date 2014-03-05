466cdb256f3da09f982d638484df86367676b5d7
Laurent Jouanneau <ljouanneau@gmail.com>
2014-03-05 07:47:01 +0100

    Move jClasses, jClassBinding, jIncluder to Jelix/Core

---
+++ lib/Jelix/Legacy/core/jISimpleCompiler.php
@@ -0,0 +1,16 @@
+<?php
+/**
+* @package  Jelix\Legacy
+* @author   Laurent Jouanneau
+* @contributor
+* @copyright 2014 Laurent Jouanneau
+* @link     http://www.jelix.org
+* @licence  MIT
+*/
+
+/**
+ * dummy class for compatibility
+ * @see \Jelix\Core\Includer\SimpleCompilerInterface
+ * @deprecated
+ */
+interface jISimpleCompiler extends \Jelix\Core\Includer\SimpleCompilerInterface { }
--- lib/Jelix/Legacy/init.php
+++ lib/Jelix/Legacy/init.php
@@ -12,10 +12,14 @@ $GLOBALS['JELIX_LEGACY_CLASSES'] = array(
     "jApp" => __DIR__.'/core/jApp.php',
     "jAppManager" => __DIR__.'/core/jAppManager.php',
     "jAutoloader" => __DIR__.'/core/jAutoloader.php',
+    "jClasses" => __DIR__.'/utils/jClasses.php',
+    "jClassBinding" => __DIR__.'/utils/jClassBinding.php',
     "jConfig" => __DIR__.'/core/jConfig.php',
     "jConfigAutoloader" => __DIR__.'/core/jConfigAutoloader.php',
     "jConfigCompiler" => __DIR__.'/core/jConfigCompiler.php',
-    
+    "jIMultiFileCompiler" => __DIR__.'/core/jIMultiFileCompiler.php',
+    "jIncluder" => __DIR__.'/core/jIncluder.php',
+    "jISimpleCompiler" => __DIR__.'/core/jISimpleCompiler.php',
 );
 
 
