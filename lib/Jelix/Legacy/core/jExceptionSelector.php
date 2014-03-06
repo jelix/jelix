e1e4f2870176e8357f7a8dd4e70d8431998ae766
Laurent Jouanneau <ljouanneau@gmail.com>
2014-03-06 22:28:36 +0100

    Move jSelectorFactory, jISelector and jExceptionSelector to Jelix/Core

---
+++ lib/Jelix/Legacy/core/jExceptionSelector.php
@@ -0,0 +1,16 @@
+<?php
+/**
+* @package  Jelix\Legacy
+* @author   Laurent Jouanneau
+* @contributor
+* @copyright 2014 Laurent Jouanneau
+* @link     http://www.jelix.org
+* @licence  MIT
+*/
+
+/**
+ * dummy class for compatibility
+ * @see \Jelix\Core\Selector\Exception
+ * @deprecated
+ */
+class jExceptionSelector extends \Jelix\Core\Selector\Exception { }
