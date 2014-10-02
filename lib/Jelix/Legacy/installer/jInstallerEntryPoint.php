5da26415167812b5e45f77909334be3bc8b6aee0
Laurent Jouanneau <ljouanneau@gmail.com>
2014-10-02 23:21:17 +0200

    Namespacify jInstaller and jInstallerEntryPoint

---
+++ lib/Jelix/Legacy/installer/jInstallerEntryPoint.php
@@ -0,0 +1,16 @@
+<?php
+/**
+* @package  Jelix\Legacy
+* @author   Laurent Jouanneau
+* @contributor
+* @copyright 2014 Laurent Jouanneau
+* @link     http://www.jelix.org
+* @licence  MIT
+*/
+
+/**
+ * dummy class for compatibility
+ * @see \Jelix\Installer\EntryPoint
+ * @deprecated
+ */
+class jInstallerEntryPoint extends \Jelix\Installer\EntryPoint { }
