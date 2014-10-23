6220c6bd60fcd062181da974da85233408ddc4c8
Laurent Jouanneau <ljouanneau@gmail.com>
2014-10-23 10:54:33 +0200

    vagrant - updatesrc.sh: reset temp directory at each sources update

--- dev/updatesrc.sh
+++ dev/updatesrc.sh
@@ -6,4 +6,8 @@ cd $BASEDIR
 php build/buildjelix.php dev/jelix.ini
 php build/buildapp.php dev/testapp.ini
 
-cd $CURRENTDIR
+cd dev/
+
+if [ -d _build/temp/testapp/www/ ]; then
+    rm -rf _build/temp/testapp/www/*
+fi
