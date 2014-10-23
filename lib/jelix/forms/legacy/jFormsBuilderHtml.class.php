07718de23b4b15303ca3fa4878298839ef3e38b4
Laurent Jouanneau <ljouanneau@gmail.com>
2014-10-23 16:33:07 +0200

    jForms: choice items (li elements)  have now an id and a class
    The class indicates if the item is selected or not: jforms-selected
    and jforms-notselected

--- lib/jelix/forms/legacy/jFormsBuilderHtml.class.php
+++ lib/jelix/forms/legacy/jFormsBuilderHtml.class.php
@@ -1047,7 +1047,7 @@ class jFormsBuilderHtml extends jFormsBuilderBase {
         foreach( $ctrl->items as $itemName=>$listctrl){
             if (!$ctrl->isItemActivated($itemName))
                 continue;
-            echo '<li><label><input';
+            echo '<li id="'.$id.$itemName.'_item"><label><input';
             $attr['id'] = $id.$i;
             $attr['value'] = $itemName;
             if ($itemName==$value)
