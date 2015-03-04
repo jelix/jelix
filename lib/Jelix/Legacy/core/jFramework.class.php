b1785b9e740ea5582384851dc31ab1ab4ccc31cb
Laurent Jouanneau <ljouanneau@gmail.com>
2015-03-04 22:53:47 +0100

    Declare mapping of legacy jFramework to \Jelix\Core\Framework

---
+++ lib/Jelix/Legacy/core/jFramework.class.php
@@ -0,0 +1,16 @@
+<?php
+/**
+* @package  Jelix\Legacy
+* @author   Laurent Jouanneau
+* @contributor
+* @copyright 2015 Laurent Jouanneau
+* @link     http://www.jelix.org
+* @licence  MIT
+*/
+
+/**
+ * dummy class for compatibility
+ * @see \Jelix\Core\Framework
+ * @deprecated
+ */
+class jFramework extends \Jelix\Core\Framework { }
