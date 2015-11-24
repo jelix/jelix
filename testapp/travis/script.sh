41744148be698c9c1b35e54fd859adfea978feb4
Laurent Jouanneau <dev@ljouanneau.com>
2015-11-24 22:12:26 +0100

    [travis] some comments..

--- testapp/travis/script.sh
+++ testapp/travis/script.sh
@@ -5,8 +5,11 @@ cd testapp/tests-jelix/
 EXITCODE=$?
 
 if [ $EXITCODE != 0 ]; then
+    echo "Tests failed. See errors log:"
     if [ -f ../var/log/errors.log ]; then
         cat ../var/log/errors.log
+    else
+        echo "/!\\ no errors.log file"
     fi
 fi
 exit $EXITCODE
