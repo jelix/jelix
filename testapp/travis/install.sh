489a71373947fa59b510ac67a79baaeca822ff1c
Laurent Jouanneau <dev@ljouanneau.com>
2015-11-24 21:17:54 +0100

    [travis] install adminapp + show errors.log file

--- testapp/travis/install.sh
+++ testapp/travis/install.sh
@@ -2,6 +2,8 @@
 
 cp -a testapp/var/config/profiles.ini.php.dist testapp/var/config/profiles.ini.php
 cp -a testapp/var/config/localconfig.ini.php.dist testapp/var/config/localconfig.ini.php
+cp -a testapp/adminapp/var/config/profiles.ini.php.dist testapp/adminapp/var/config/profiles.ini.php
+cp -a testapp/adminapp/var/config/localconfig.ini.php.dist testapp/adminapp/var/config/localconfig.ini.php
 
 
 composer install --prefer-source
@@ -9,3 +11,7 @@ cd testapp
 composer install --prefer-source
 
 php install/installer.php
+
+cd adminapp/install
+php install/installer.php
+
--- testapp/travis/script.sh
+++ testapp/travis/script.sh
@@ -2,4 +2,11 @@
 
 cd testapp/tests-jelix/
 ../vendor/bin/phpunit
-exit $?
+EXITCODE=$?
+
+if [ $EXITCODE != 0 ]; then
+    if [ -f ../var/log/errors.log ]; then
+        cat ../var/log/errors.log
+    fi
+fi
+exit $EXITCODE
