03924ef74e24ef0bc47a89edb9eed29c0371bba2
Laurent Jouanneau <dev@ljouanneau.com>
2016-02-02 16:46:22 +0100

    Testapp: use a class datasource for tests
    Refs #221

---
+++ testapp/modules/testapp/classes/geo/regions.class.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once (JELIX_LIB_PATH.'forms/jFormsDatasource.class.php');
+
+class regions implements jIFormsDatasource
+{
+  protected $formId = 0;
+
+  protected $data = array(
+    'finistere'=> 'Finistère',
+    'touraine'=> 'Touraine',
+    'polynesia'=> 'Polynésie',
+    );
+
+  function __construct($id)
+  {
+    $this->formId = $id;
+  }
+
+  public function getData($form)
+  {
+    return ($this->data);
+  }
+
+  public function getLabel($key)
+  {
+    if(isset($this->data[$key]))
+      return $this->data[$key];
+    else
+      return null;
+  }
+
+}
