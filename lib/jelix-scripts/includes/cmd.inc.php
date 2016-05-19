a70309a99c93ac4b5093be3d9594826b968b0d2d
Laurent Jouanneau <dev@ljouanneau.com>
2016-05-19 18:59:56 +0200

    Symfony console migration: createlangpackage

--- lib/jelix-scripts/includes/cmd.inc.php
+++ lib/jelix-scripts/includes/cmd.inc.php
@@ -39,6 +39,7 @@ $application->add(new CreateClassFromDao($jelixScriptConfig));
 $application->add(new CreateModule($jelixScriptConfig));
 $application->add(new CreateEntryPoint($jelixScriptConfig));
 $application->add(new CreateForm($jelixScriptConfig));
+$application->add(new CreateLangPackage($jelixScriptConfig));
 $application->add(new CreateZone($jelixScriptConfig));
 $application->add(new ClearTemp($jelixScriptConfig));
 $application->add(new CloseApp($jelixScriptConfig));
