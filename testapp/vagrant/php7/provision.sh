9240dd04f4c40019d6b3f2a4a400024b9d54dfdb
Laurent Jouanneau <dev@ljouanneau.com>
2016-09-22 17:10:26 +0200

    vagrant: fix nginx config again

--- testapp/vagrant/php7/provision.sh
+++ testapp/vagrant/php7/provision.sh
@@ -8,7 +8,7 @@ APPDIR="$ROOTDIR/$APPNAME"
 VAGRANTDIR="$APPDIR/vagrant"
 APPHOSTNAME="testapp.local"
 APPHOSTNAME2="testapp17.local"
-FPM_SOCK="php/php7.0-fpm.sock"
+FPM_SOCK="php\\/php7.0-fpm.sock"
 POSTGRESQL_VERSION=9.5
 
 source $VAGRANTDIR/common_provision.sh
