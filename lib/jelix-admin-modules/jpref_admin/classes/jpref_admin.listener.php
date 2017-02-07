e54db78ec84a04758b8a4d32095a24238bc00a35
Laurent Jouanneau <dev@ljouanneau.com>
2017-02-07 08:57:40 +0100

    [NEW] jApp::urlJelixWWWPath(), to retrieve jelixWWWPath config param

--- lib/jelix-admin-modules/jpref_admin/classes/jpref_admin.listener.php
+++ lib/jelix-admin-modules/jpref_admin/classes/jpref_admin.listener.php
@@ -16,7 +16,7 @@ class jpref_adminListener extends jEventListener{
     function onmasteradminGetMenuContent ($event) {
         if (jAcl2::check('jprefs.prefs.list')) {
             $item = new masterAdminMenuItem('pref', jLocale::get('jpref_admin~admin.item.title'), jUrl::get('jpref_admin~prefs:index'), 50, 'system');
-            $item->icon = jApp::config()->urlengine['jelixWWWPath'] . 'design/images/cog.png';
+            $item->icon = jApp::urlJelixWWWPath() . 'design/images/cog.png';
             $event->add($item);
         }
     }
--- lib/jelix-scripts/templates/app/responses/adminHtmlResponse.class.php.tpl
+++ lib/jelix-scripts/templates/app/responses/adminHtmlResponse.class.php.tpl
@@ -18,7 +18,7 @@ class adminHtmlResponse extends jResponseHtml {
         parent::__construct();
 
         // Include your common CSS and JS files here
-        $this->addCSSLink(jApp::config()->urlengine['jelixWWWPath'].'design/master_admin.css');
+        $this->addCSSLink(jApp::urlJelixWWWPath().'design/master_admin.css');
     }
 
     protected function doAfterActions() {
