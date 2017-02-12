41aa0ab6209cbcf61b1355c772c61d22f4daa674
Laurent Jouanneau <dev@ljouanneau.com>
2017-02-12 16:58:35 +0100

    Upgrade jQuery-UI to 1.12.1
    Since jQuery-UI does not provides separate files for
    each widget, we provide from now a build of jQueryUI
    containing only the datepicker widget, because the default
    jQuery-UI is too heavy (500KB!!) for a datepicker.
    
    If the user want a different version of jQueyr with more
    features, he has to put files of jQuery into its app
    and change the webassets configuration.

--- lib/jelix/core/defaultconfig.ini.php
+++ lib/jelix/core/defaultconfig.ini.php
@@ -353,11 +353,17 @@ useSet=common
 [webassets_common]
 jquery.js = "$jelix/jquery/jquery.min.js"
 
-jquery_ui.js = "$jelix/jquery/ui/jquery-ui-core-widg-mous-posi.custom.min.js"
-jquery_ui.css = "$jelix/jquery/themes/base/jquery.ui.all.css"
+; In Jelix we need only the datepicker from jQueryUI.
+; So JQuery UI bundled into Jelix has been built with only the datepicker widget
+; To have more jQueryUI features, download your own version from http://jqueryui.com/download/,
+; put files into you www/ of your app, and list them here.
+; Do not overwrite files into lib/jelix-www/ !
+jquery_ui.js = "$jelix/jquery/ui-datepicker/jquery-ui.min.js"
+jquery_ui.css[] = "$jelix/jquery/ui-datepicker/jquery-ui.min.css"
+jquery_ui.css[] = "$jelix/jquery/ui-datepicker/jquery-ui.structure.min.css"
+jquery_ui.css[] = "$jelix/jquery/ui-datepicker/jquery-ui.theme.min.css"
 jquery_ui.require = jquery
 
-jforms_html.js[]= "$jelix/jquery/include/jquery.include.js"
 jforms_html.js[]= "$jelix/js/jforms_jquery.js"
 jforms_html.css= "$jelix/design/jform.css"
 jforms_html.require = jquery
@@ -366,8 +372,7 @@ jforms_html_light.js= "$jelix/js/jforms_light.js"
 jforms_html_light.css= "$jelix/design/jform.css"
 
 jforms_datepicker_default.css=
-jforms_datepicker_default.js[]="$jelix/jquery/ui/jquery.ui.datepicker.min.js"
-jforms_datepicker_default.js[]="$jelix/jquery/ui/i18n/jquery.ui.datepicker-$lang.js"
+jforms_datepicker_default.js[]="$jelix/jquery/ui-datepicker/i18n/datepicker-$lang.js"
 jforms_datepicker_default.js[]="$jelix/js/jforms/datepickers/default/init.js"
 jforms_datepicker_default.require=jquery_ui
 
@@ -386,7 +391,6 @@ jforms_htmleditor_wymbasic.js[]="$jelix/js/jforms/htmleditors/wymeditor_basic.js
 jforms_htmleditor_wymbasic.require=jquery
 
 jforms_htmleditor_wymbasic.skin.default.css="$jelix/wymeditor/skins/default/skin.css"
-
 
 jforms_htmleditor_ckdefault.js[]="$jelix/ckeditor/ckeditor.js"
 jforms_htmleditor_ckdefault.js[]="$jelix/js/jforms/htmleditors/ckeditor_default.js"
