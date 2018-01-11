c0c174363b5f3a5571b4b6aee87506db796d1608
Laurent Jouanneau <dev@ljouanneau.com>
2018-01-11 01:03:15 +0100

    Update comments in auth.coord.ini templates

--- lib/jelix-modules/jauth/install/var/config/authsw.coord.ini.php
+++ lib/jelix-modules/jauth/install/var/config/authsw.coord.ini.php
@@ -95,7 +95,9 @@ dao = ""
 ; profile to use for jDb 
 profile = ""
 
-; name of the php function to crypt the password in the database
+; name of the php function used to hash password in the database
+; It is deprecated but still used to convert password hash
+; to new hashes with password_hash_method
 password_crypt_function = sha1
 ; if you want to use a salt with sha1:
 ;password_crypt_function = "1:sha1WithSalt"
@@ -106,7 +108,9 @@ password_crypt_function = sha1
 ; selector of the class
 class = ""
 
-; name of the php function to crypt the password in the database
+; name of the php function used to hash password
+; It is deprecated but still used to convert password hash
+; to new hashes with password_hash_method
 password_crypt_function = sha1
 ; if you want to use a salt with sha1:
 ;password_crypt_function = "1:sha1WithSalt"
