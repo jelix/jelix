8df21cafcfaa0cd7147f9644a556a68c06a4cda8
Laurent Jouanneau <dev@ljouanneau.com>
2019-05-21 16:41:36 +0200

    Fix tests

--- lib/Jelix/Routing/Router.php
+++ lib/Jelix/Routing/Router.php
@@ -530,7 +530,7 @@ class Router
         // the config compiler has removed all deactivated plugins
         // so we don't have to check if the value $conf is empty or not
         $conf = $config->coordplugins[$pluginName];
-        if ($conf !== 1 && $conf !== true) {
+        if ($conf !== 1 && $conf !== true && !is_numeric($conf)) {
             // the path to the coordplugin conf has already been processed
             // by the config compiler, and is now a relative path to the app
             $pluginConfFile = App::appPath($conf);
--- testapp/tests-jelix/jelix/installer/moduleParametersTest.php
+++ testapp/tests-jelix/jelix/installer/moduleParametersTest.php
@@ -1,7 +1,4 @@
 <?php
-require_once (JELIX_LIB_PATH.'installer/jInstallerApplication.class.php');
-require_once (JELIX_LIB_PATH.'core/jConfigCompiler.class.php');
-
 
 class moduleParametersTest extends PHPUnit_Framework_TestCase
 {
