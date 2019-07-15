f42eab48dd67976b4d410f9800e2ca9d53dcfb89
Laurent Jouanneau <dev@ljouanneau.com>
2019-07-15 11:13:26 +0200

    Fix parameters serialization for installers
    Original values with array having a single value, should be unserialized as
    an array.
    
    Serialization for ini files are now not a fully serialized string but provides
    an array.

--- testapp/tests-jelix/jelix/installer/moduleParametersTest.php
+++ testapp/tests-jelix/jelix/installer/moduleParametersTest.php
@@ -6,7 +6,7 @@ require_once (JELIX_LIB_PATH.'core/jConfigCompiler.class.php');
 class moduleParametersTest extends PHPUnit_Framework_TestCase
 {
 
-    function getSerialized() {
+    function getSerializedToUnserialized() {
         return array(
             array(
                 '',
@@ -37,7 +37,35 @@ class moduleParametersTest extends PHPUnit_Framework_TestCase
                 array('foo'=>'abc', 'bar'=>array('a','b','c'), 'baz'=>2)
             ),
             array(
+                'foo=abc;bar=[a,b,c];baz=2',
+                array('foo'=>'abc', 'bar'=>array('a','b','c'), 'baz'=>2)
+            ),
+            array(
+                'foo=abc;bar=[a];baz=2',
+                array('foo'=>'abc', 'bar'=>array('a'), 'baz'=>2)
+            ),
+            array(
                 'foo=false',
+                array('foo'=>false)
+            ),
+            array(
+                array('foo'=>'true', 'bar'=>true),
+                array('foo'=>true, 'bar'=>true)
+            ),
+            array(
+                array('foo'=>'abc'),
+                array('foo'=>'abc')
+            ),
+            array(
+                array('foo'=>'abc', 'bar'=>true, 'baz'=>2),
+                array('foo'=>'abc', 'bar'=>true, 'baz'=>2)
+            ),
+            array(
+                array('foo'=>'abc', 'bar'=>'[a,b,c]', 'baz'=>2),
+                array('foo'=>'abc', 'bar'=>array('a','b','c'), 'baz'=>2)
+            ),
+            array(
+                array('foo'=>'false'),
                 array('foo'=>false)
             ),
         );
@@ -45,7 +73,7 @@ class moduleParametersTest extends PHPUnit_Framework_TestCase
 
 
     /**
-     * @dataProvider getSerialized
+     * @dataProvider getSerializedToUnserialized
      */
     function testUnserialize($serialized, $expected) {
         $this->assertEquals(
@@ -54,14 +82,113 @@ class moduleParametersTest extends PHPUnit_Framework_TestCase
         );
     }
 
+    function getUnserializedToSerializedAsString() {
+        return array(
+            array(
+                array(),
+                ''
+            ),
+            array(
+                array('foo'=>true),
+                'foo'
+            ),
+            array(
+                array('foo'=>''),
+                'foo='
+            ),
+            array(
+                array('foo'=>true, 'bar'=>true),
+                'foo;bar'
+            ),
+            array(
+                array('foo'=>true, 'bar'=>'true'),
+                'foo;bar',
+            ),
+            array(
+                array('foo'=>'abc'),
+                'foo=abc'
+            ),
+            array(
+                array('foo'=>'abc', 'bar'=>true, 'baz'=>2),
+                'foo=abc;bar;baz=2',
+            ),
+            array(
+                array('foo'=>'abc', 'bar'=>array('a','b','c'), 'baz'=>2),
+                'foo=abc;bar=[a,b,c];baz=2'
+            ),
+            array(
+                array('foo'=>'abc', 'bar'=>array('a'), 'baz'=>2),
+                'foo=abc;bar=[a];baz=2',
+            ),
+            array(
+                array('foo'=>false),
+                'foo=false',
+            ),
+        );
+    }
 
     /**
-     * @dataProvider getSerialized
+     * @dataProvider getUnserializedToSerializedAsString
      */
-    function testSerialize($expectedSerialized, $data) {
+    function testSerializeAsString($data, $expectedSerialized) {
         $this->assertEquals(
             $expectedSerialized,
-            \Jelix\Installer\ModuleStatus::serializeParameters($data)
+            \Jelix\Installer\ModuleStatus::serializeParametersAsString($data)
+        );
+    }
+
+    function getUnserializedToSerializedAsArray() {
+        return array(
+            array(
+                array(),
+                array(),
+            ),
+            array(
+                array('foo'=>true),
+                array('foo'=>'true'),
+            ),
+            array(
+                array('foo'=>''),
+                array('foo'=>''),
+            ),
+            array(
+                array('foo'=>true, 'bar'=>true),
+                array('foo'=>'true', 'bar'=>'true'),
+            ),
+            array(
+                array('foo'=>true, 'bar'=>'true'),
+                array('foo'=>'true', 'bar'=>'true'),
+            ),
+            array(
+                array('foo'=>'abc'),
+                array('foo'=>'abc'),
+            ),
+            array(
+                array('foo'=>'abc', 'bar'=>true, 'baz'=>2),
+                array('foo'=>'abc', 'bar'=>'true', 'baz'=>2),
+            ),
+            array(
+                array('foo'=>'abc', 'bar'=>array('a','b','c'), 'baz'=>2),
+                array('foo'=>'abc', 'bar'=>'[a,b,c]', 'baz'=>2),
+            ),
+            array(
+                array('foo'=>'abc', 'bar'=>array('a'), 'baz'=>2),
+                array('foo'=>'abc', 'bar'=>'[a]', 'baz'=>2),
+            ),
+            array(
+                array('foo'=>false),
+                array('foo'=>'false'),
+            ),
+        );
+    }
+
+    /**
+     * @dataProvider getUnserializedToSerializedAsArray
+     */
+    function testSerializeAsArray($data, $expectedSerialized) {
+        $this->assertEquals(
+            $expectedSerialized,
+            \Jelix\Installer\ModuleStatus::serializeParametersAsArray($data)
         );
     }
 }
