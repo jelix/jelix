acdde6f4eaf833fc19de8420ec948d41b617cfe8
Laurent Jouanneau <dev@ljouanneau.com>
2020-01-01 17:43:12 +0100

    Remove tests for CasperJS
    This is a dead project, and we don't use it finally

--- testapp/tests-jelix/casperjs/pre.js
+++
@@ -1,9 +0,0 @@
-
-var testUrl = 'http://testapp17.local';
-
-if (casper.cli.has("testurl"))
-    testUrl = casper.cli.get('testurl');
-
-
-casper.echo("Tests will be done on "+testUrl);
-casper.test.done();
