952c775fcd747f34dcc89e925c457cecd0f8050b
Laurent Jouanneau <dev@ljouanneau.com>
2020-10-11 09:19:54 +0200

    [testapp] Fix php 7.4 vagrant provision

--- testapp/vagrant/php7.4/provision.sh
+++ testapp/vagrant/php7.4/provision.sh
@@ -5,10 +5,10 @@ MYSQL_VERSION=""
 PHP_VERSION="7.4"
 APPNAME="testapp"
 APPDIR="$ROOTDIR/$APPNAME"
-VAGRANTDIR="/vagrantscripts"
-APPHOSTNAME="testapp16.local"
+VAGRANTDIR="$APPDIR/vagrant"
+APPHOSTNAME="testapp17.local"
 APPHOSTNAME2=""
-LDAPCN="testapp16"
+LDAPCN="testapp17"
 FPM_SOCK="php\\/php7.4-fpm.sock"
 POSTGRESQL_VERSION=11
 
