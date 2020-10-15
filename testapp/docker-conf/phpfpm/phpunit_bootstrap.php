6aa69794ae89a869166239f5ed7cbd006a2cd9f2
Laurent Jouanneau <dev@ljouanneau.com>
2020-10-15 17:22:07 +0200

    [tests] testapp is now on the localhost with port 8817

--- testapp/docker-conf/phpfpm/phpunit_bootstrap.php
+++ testapp/docker-conf/phpfpm/phpunit_bootstrap.php
@@ -3,7 +3,9 @@
 
 define('TESTAPP_URL', 'http://testapp17.local/');
 define('TESTAPP_URL_HOST', 'testapp17.local');
+define('TESTAPP_URL_HOST_PORT', 'testapp17.local:'.getenv('TESTAPP_WEB_PORT'));
 define('TESTAPP_HOST', 'testapp17.local');
+define('TESTAPP_PORT', getenv('TESTAPP_WEB_PORT'));
 
 define('TESTAPP_REDIS_HOST', 'redis');
 define('TESTAPP_LDAP_HOST', 'openldap');
--- testapp/travis/phpunit_bootstrap.php
+++ testapp/travis/phpunit_bootstrap.php
@@ -3,7 +3,9 @@
 
 define('TESTAPP_URL', 'http://testapp17.local/');
 define('TESTAPP_URL_HOST', 'testapp17.local');
+define('TESTAPP_URL_HOST_PORT', 'testapp17.local');
 define('TESTAPP_HOST', 'testapp17.local');
+define('TESTAPP_PORT', '');
 
 define('TESTAPP_REDIS_HOST', 'localhost');
 define('TESTAPP_LDAP_HOST', 'localhost');
