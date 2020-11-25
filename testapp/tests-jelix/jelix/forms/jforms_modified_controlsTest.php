6768480da13999bf87177585e2efed2314530905
Laurent Jouanneau <dev@ljouanneau.com>
2020-11-25 14:51:48 +0100

    Fix comparison of values in the jForms modified check

--- testapp/tests-jelix/jelix/forms/jforms_modified_controlsTest.php
+++ testapp/tests-jelix/jelix/forms/jforms_modified_controlsTest.php
@@ -4,7 +4,7 @@
 * @subpackage  unittest module
 * @author      Bastien Jaillot
 * @contributor Laurent Jouanneau, Steven Jehannet
-* @copyright   2008 Bastien Jaillot, 2009 Laurent Jouanneau, 2010 Steven Jehannet
+* @copyright   2008 Bastien Jaillot, 2009-2020 Laurent Jouanneau, 2010 Steven Jehannet
 * @link        http://jelix.org
 * @licence     GNU Lesser General Public Licence see LICENCE file or http://www.gnu.org/licenses/lgpl.html
 */
@@ -38,15 +38,29 @@ class jforms_Modified_ControlsTest extends jUnitTestCaseDb {
         $this->form->addCtrl($ctrl);
         $this->form->setData('inputctrl', 'toto');
 
+        $ctrl = new jFormsControlInput('emptyctrl');
+        // no value
+        $ctrl->setForm($this->form);
+        $this->form->addControl($ctrl);
+
+        $ctrl = new jFormsControlInput('integerctrl');
+        $ctrl->datatype=new jDatatypeInteger();
+        $ctrl->setForm($this->form);
+        $ctrl->setData('');
+        $this->form->addControl($ctrl);
+
+
         $ctrl = new jFormsControlCheckbox('chckbxctrl');
         $ctrl->datatype=new jDatatypeBoolean();
-        $this->form->addCtrl($ctrl, false);
+        $this->form->addControl($ctrl);
         $this->form->setData('chckbxctrl', '1');
+
         $ctrl= new jFormsControlcheckbox('chckbxctrl1');
         $ctrl->defaultValue='0';
         $ctrl->valueOnCheck='1';
         $ctrl->valueOnUncheck='0';
         $this->form->addControl($ctrl);
+
         $ctrl= new jFormsControlcheckbox('chckbxctrl2');
         $ctrl->defaultValue='1';
         $ctrl->valueOnCheck='1';
@@ -57,7 +71,7 @@ class jforms_Modified_ControlsTest extends jUnitTestCaseDb {
         $ctrl->multiple=true;
         $ctrl->datasource = new jFormsStaticDatasource();
         $ctrl->datasource->data = array('a'=>'aaa', 'b'=>'bbb', 'c'=>'123', 'd'=>'456');
-        $this->form->addCtrl($ctrl, false);
+        $this->form->addControl($ctrl);
         $this->form->setData('list', array('bbb','123'));
     }
 
@@ -68,6 +82,8 @@ class jforms_Modified_ControlsTest extends jUnitTestCaseDb {
 
         $initForm = array (
           'inputctrl' => 'toto',
+          'emptyctrl' => '',
+          'integerctrl' => '',
           'chckbxctrl' => '1',
           'chckbxctrl1'=>'0',
           'chckbxctrl2'=>'1',
@@ -82,17 +98,22 @@ class jforms_Modified_ControlsTest extends jUnitTestCaseDb {
         $this->form->setData('chckbxctrl', '0');
         $newForm = array (
           'inputctrl' => 'toto',
+          'emptyctrl' => '',
+          'integerctrl' => '',
           'chckbxctrl' => '0',
           'chckbxctrl1'=>'0',
           'chckbxctrl2'=>'1',
           'list'=>array('bbb','123'),);
         $this->assertEquals($newForm, $this->form->getContainer()->data);
+
         $modifiedControls = array (
             'chckbxctrl' => '1');
         $this->assertEquals($modifiedControls, $this->form->getModifiedControls());
         $this->form->setData('inputctrl', 'tata');
         $newForm = array (
           'inputctrl' => 'tata',
+          'emptyctrl' => '',
+          'integerctrl' => '',
           'chckbxctrl' => '0',
           'chckbxctrl1'=>'0',
           'chckbxctrl2'=>'1',
@@ -107,6 +128,8 @@ class jforms_Modified_ControlsTest extends jUnitTestCaseDb {
         $this->form->setData('list', array('123'));
         $newForm = array (
           'inputctrl' => 'tata',
+          'emptyctrl' => '',
+          'integerctrl' => '',
           'chckbxctrl' => '0',
           'chckbxctrl1'=>'0',
           'chckbxctrl2'=>'1',
@@ -121,6 +144,8 @@ class jforms_Modified_ControlsTest extends jUnitTestCaseDb {
         $this->form->setData('list', array());
         $newForm = array (
           'inputctrl' => 'tata',
+          'emptyctrl' => '',
+          'integerctrl' => '',
           'chckbxctrl' => '0',
           'chckbxctrl1'=>'0',
           'chckbxctrl2'=>'1',
@@ -135,6 +160,8 @@ class jforms_Modified_ControlsTest extends jUnitTestCaseDb {
         // despite all changes : originalData hasn't changed
         $initForm = array (
           'inputctrl' => 'toto',
+          'emptyctrl' => '',
+          'integerctrl' => '',
           'chckbxctrl' => '1',
           'chckbxctrl1'=>'0',
           'chckbxctrl2'=>'1',
@@ -145,10 +172,17 @@ class jforms_Modified_ControlsTest extends jUnitTestCaseDb {
 
     function testModifiedInput() {
         $this->form->setData('inputctrl', null);
+        $this->form->setData('integerctrl', '');
         $this->form->initModifiedControlsList();
 
         $this->form->setData('inputctrl', '');
         $modifiedControls = array ();
+        $this->assertEquals($modifiedControls, $this->form->getModifiedControls());
+
+        $this->form->setData('inputctrl', '0');
+        $modifiedControls = array (
+            'inputctrl' => null
+        );
         $this->assertEquals($modifiedControls, $this->form->getModifiedControls());
 
         $this->form->setData('inputctrl', 'hey');
@@ -157,6 +191,127 @@ class jforms_Modified_ControlsTest extends jUnitTestCaseDb {
         $this->assertEquals($modifiedControls, $this->form->getModifiedControls());
     }
 
+    function testModifiedIntegerInput() {
+        // with initial empty value
+
+        $this->form->setData('inputctrl', null);
+        $this->form->setData('integerctrl', '');
+        $this->form->initModifiedControlsList();
+
+        $this->form->setData('integerctrl', '');
+        $modifiedControls = array ();
+        $this->assertEquals($modifiedControls, $this->form->getModifiedControls());
+
+        $this->form->setData('integerctrl', '0');
+        $modifiedControls = array (
+            'integerctrl' => ''
+        );
+        $this->assertEquals($modifiedControls, $this->form->getModifiedControls());
+
+        $this->form->setData('integerctrl', 0);
+        $modifiedControls = array (
+            'integerctrl' => ''
+        );
+        $this->assertEquals($modifiedControls, $this->form->getModifiedControls());
+
+
+        $this->form->setData('integerctrl', '10');
+        $modifiedControls = array (
+            'integerctrl' => '');
+        $this->assertEquals($modifiedControls, $this->form->getModifiedControls());
+
+        $this->form->setData('integerctrl', 10);
+        $modifiedControls = array (
+            'integerctrl' => '');
+        $this->assertEquals($modifiedControls, $this->form->getModifiedControls());
+
+        // with initial 0 value
+
+        $this->form->setData('integerctrl', 0);
+        $this->form->initModifiedControlsList();
+
+        $this->form->setData('integerctrl', '');
+        $modifiedControls = array (
+            'integerctrl' => 0
+        );
+        $this->assertEquals($modifiedControls, $this->form->getModifiedControls());
+
+        $this->form->setData('integerctrl', '0');
+        $modifiedControls = array ();
+        $this->assertEquals($modifiedControls, $this->form->getModifiedControls());
+
+        $this->form->setData('integerctrl', 0);
+        $modifiedControls = array ();
+        $this->assertEquals($modifiedControls, $this->form->getModifiedControls());
+
+        $this->form->setData('integerctrl', '10');
+        $modifiedControls = array (
+            'integerctrl' => 0);
+        $this->assertEquals($modifiedControls, $this->form->getModifiedControls());
+
+        $this->form->setData('integerctrl', 10);
+        $modifiedControls = array (
+            'integerctrl' => 0);
+        $this->assertEquals($modifiedControls, $this->form->getModifiedControls());
+
+        // with initial '0' value
+
+        $this->form->setData('integerctrl', '0');
+        $this->form->initModifiedControlsList();
+
+        $this->form->setData('integerctrl', '');
+        $modifiedControls = array (
+            'integerctrl' => '0'
+        );
+        $this->assertEquals($modifiedControls, $this->form->getModifiedControls());
+
+        $this->form->setData('integerctrl', '0');
+        $modifiedControls = array ();
+        $this->assertEquals($modifiedControls, $this->form->getModifiedControls());
+
+        $this->form->setData('integerctrl', 0);
+        $modifiedControls = array ();
+        $this->assertEquals($modifiedControls, $this->form->getModifiedControls());
+
+        $this->form->setData('integerctrl', '10');
+        $modifiedControls = array (
+            'integerctrl' => '0');
+        $this->assertEquals($modifiedControls, $this->form->getModifiedControls());
+
+        $this->form->setData('integerctrl', 10);
+        $modifiedControls = array (
+            'integerctrl' => '0');
+        $this->assertEquals($modifiedControls, $this->form->getModifiedControls());
+
+        // with initial '10' value
+
+        $this->form->setData('integerctrl', '10');
+        $this->form->initModifiedControlsList();
+
+        $this->form->setData('integerctrl', '');
+        $modifiedControls = array (
+            'integerctrl' => '10'
+        );
+        $this->assertEquals($modifiedControls, $this->form->getModifiedControls());
+
+        $this->form->setData('integerctrl', '0');
+        $modifiedControls = array (
+            'integerctrl' => '10');
+        $this->assertEquals($modifiedControls, $this->form->getModifiedControls());
+
+        $this->form->setData('integerctrl', 0);
+        $modifiedControls = array (
+            'integerctrl' => '10');
+        $this->assertEquals($modifiedControls, $this->form->getModifiedControls());
+
+        $this->form->setData('integerctrl', '10');
+        $modifiedControls = array ();
+        $this->assertEquals($modifiedControls, $this->form->getModifiedControls());
+
+        $this->form->setData('integerctrl', 10);
+        $modifiedControls = array ();
+        $this->assertEquals($modifiedControls, $this->form->getModifiedControls());
+    }
 
     function testModifiedNumericalValue() {
         $this->form->setData('chckbxctrl', 0);
