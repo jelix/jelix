af8894d9ebd7d6171e891917c45e0140f067b4c0
Laurent Jouanneau <dev@ljouanneau.com>
2022-09-14 14:28:13 +0200

    Restore the jFormsDatasource.class.php file to avoid include breaks.

---
+++ lib/jelix/forms/jFormsDatasource.class.php
@@ -0,0 +1,8 @@
+<?php
+
+// deprecated file.
+// It is here only to not break includes of this file.
+// All classes that were into this file are now into their own files
+// and are autoloaded.
+
+trigger_error("Please don't include jFormsDatasource.class.php anymore", E_USER_DEPRECATED);
