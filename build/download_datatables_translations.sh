c6d83b9a2a42c55b6e753a2c84d7481a3873b2d4
Laurent Jouanneau <dev@ljouanneau.com>
2023-05-30 14:22:14 +0200

    Upgrade Datatables to 1.13.4

--- build/download_datatables_translations.sh
+++ build/download_datatables_translations.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 LANG_LIST="cs=cs_CZ de-DE=de_DE el=el_GR en-GB=en_US en-GB=en_GB es-ES=es_ES eu=eu_ES fi=fi_FI fr-FR=fr_FR gl=gl_ES hu=hu_HU it-IT=it_IT ja=ja_JP nl-NL=nl_NL pl=pl_PL pt-BR=pt_BR pt-PT=pt_PT ro=ro_RO ru=ru_RU sl=sl_SI sb-SE=sv_SE sk=sk_SK uk=uk_UA"
+DTVERSION=1.13.4
 
 for LANGDEF in $LANG_LIST; do
 
@@ -9,7 +10,7 @@ for LANGDEF in $LANG_LIST; do
   echo $DT_FIC " -> " $JLX_FIC
   TARGET_NEW=../lib/jelix-www/datatables/i18n/$JLX_FIC.new
   TARGET=../lib/jelix-www/datatables/i18n/$JLX_FIC
-  wget https://cdn.datatables.net/plug-ins/1.12.1/i18n/$DT_FIC.json -O $TARGET_NEW
+  wget https://cdn.datatables.net/plug-ins/$DTVERSION/i18n/$DT_FIC.json -O $TARGET_NEW
   if [ -f "$TARGET_NEW" ]; then
 
     CONTENT=$(cat $TARGET_NEW)
