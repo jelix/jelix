511cf3541fd0b5b6769afebb2485c22283fec58f
Laurent Jouanneau <dev@ljouanneau.com>
2023-10-27 11:47:56 +0200

    Fix jAuth: regenerate the encryption key for the cookie "remember me" at each upgrade
    in particularly when upgrading from Jelix 1.6.

---
+++ lib/jelix-modules/jauth/install/upgrade.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @package     jelix
+ * @subpackage  jauth module
+ *
+ * @author     Laurent Jouanneau
+ * @copyright  2023 Laurent Jouanneau
+ *
+ * @see       https://www.jelix.org
+ * @licence    GNU Lesser General Public Licence see LICENCE file or http://www.gnu.org/licenses/lgpl.html
+ */
+class jauthModuleUpgrader extends \Jelix\Installer\Module\Installer
+{
+    public function install(Jelix\Installer\Module\API\InstallHelpers $helpers)
+    {
+        // it's a good thing to recreate the key at each upgrade, to increase the security
+        // and to be sure that the value of persistant_encryption_key has been generated with the following code.
+        $cryptokey = \Defuse\Crypto\Key::createNewRandomKey();
+        $key = $cryptokey->saveToAsciiSafeString();
+        $helpers->getLiveConfigIni()->setValue('persistant_encryption_key', $key, 'coordplugin_auth');
+    }
+}
