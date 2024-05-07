08296f526544ed3ed08bf24cc268ed4a6a590c40
Laurent Jouanneau <dev@ljouanneau.com>
2024-05-07 14:37:22 +0200

    Release Jelix 1.8.9

--- lib/jelix/init.php
+++ lib/jelix/init.php
@@ -23,7 +23,7 @@
  * @deprecated
  * @see jFramework::version()
  */
-define('JELIX_VERSION', '1.8.9-pre');
+define('JELIX_VERSION', '1.8.9');
 
 /*
  * base of namespace path used in xml files of jelix
