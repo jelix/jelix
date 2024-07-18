fcd3a534b57bd0c790f804e83aae73d27d4c80df
Laurent Jouanneau <dev@ljouanneau.com>
2024-07-18 16:01:52 +0200

    Bumped version to  1.8.11-rc.3

--- lib/jelix/init.php
+++ lib/jelix/init.php
@@ -23,7 +23,7 @@
  * @deprecated
  * @see jFramework::version()
  */
-define('JELIX_VERSION', '1.8.11-rc.2');
+define('JELIX_VERSION', '1.8.11-rc.3');
 
 /*
  * base of namespace path used in xml files of jelix
