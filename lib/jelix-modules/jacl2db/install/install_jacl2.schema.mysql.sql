23ca3e0426a528d5ae670fd1657999426f1d285f
Raphaël Martin <rmartin@3liz.com>
2025-01-09 17:37:19 +0100

    increase length for login and id_aclgrp (because of __priv_<login>) in acl2db for sql launch by install.php and create upgrade script

--- lib/jelix-modules/jacl2db/install/install_jacl2.schema.mysql.sql
+++ lib/jelix-modules/jacl2db/install/install_jacl2.schema.mysql.sql
@@ -1,17 +1,17 @@
 
 -- Liste des groupes
 CREATE TABLE  IF NOT EXISTS `%%PREFIX%%jacl2_group` (
-  `id_aclgrp` varchar(60) NOT NULL,
+  `id_aclgrp` varchar(330) NOT NULL,
   `name` varchar(150) NOT NULL default '',
   `grouptype` tinyint(4) NOT NULL default '0',
-  `ownerlogin` varchar(50) default NULL,
+  `ownerlogin` varchar(320) default NULL,
   PRIMARY KEY  (`id_aclgrp`)
 ) DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- liste des groupes associés à chaque utilisateur
 CREATE TABLE IF NOT EXISTS `%%PREFIX%%jacl2_user_group` (
-  `login` varchar(50) NOT NULL,
-  `id_aclgrp` varchar(60) NOT NULL,
+  `login` varchar(320) NOT NULL,
+  `id_aclgrp` varchar(330) NOT NULL,
   PRIMARY KEY (`login`,`id_aclgrp`)
 ) DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
@@ -28,7 +28,7 @@ CREATE TABLE IF NOT EXISTS `%%PREFIX%%jacl2_subject` (
 -- valeurs du droit pour chaque couple sujet/groupe ou triplet sujet/groupe/ressource
 CREATE TABLE IF NOT EXISTS `%%PREFIX%%jacl2_rights` (
   `id_aclsbj` varchar(100) NOT NULL,
-  `id_aclgrp` varchar(60) NOT NULL,
+  `id_aclgrp` varchar(330) NOT NULL,
   `id_aclres` varchar(100) NOT NULL default '-',
   canceled boolean NOT NULL default 0,
   PRIMARY KEY  (`id_aclsbj`,`id_aclgrp`,`id_aclres`)
