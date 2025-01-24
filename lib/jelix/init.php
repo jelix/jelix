4386c4ed79adaa14c86032ae12cf2073167e65a9
Laurent Jouanneau <dev@ljouanneau.com>
2025-01-24 16:43:01 +0100

    Bumped version to  1.8.16-pre

--- lib/jelix/init.php
+++ lib/jelix/init.php
@@ -23,7 +23,7 @@
  * @deprecated
  * @see jFramework::version()
  */
-define('JELIX_VERSION', '1.8.15');
+define('JELIX_VERSION', '1.8.16-pre');
 
 /*
  * base of namespace path used in xml files of jelix
