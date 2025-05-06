3fcf1d29e777b24830cb079191d766abf6a5192e
Laurent Jouanneau <dev@ljouanneau.com>
2025-05-06 15:15:41 +0200

    Release Jelix 1.8.18

--- lib/jelix/init.php
+++ lib/jelix/init.php
@@ -23,7 +23,7 @@
  * @deprecated
  * @see jFramework::version()
  */
-define('JELIX_VERSION', '1.8.18-pre');
+define('JELIX_VERSION', '1.8.18');
 
 /*
  * base of namespace path used in xml files of jelix
