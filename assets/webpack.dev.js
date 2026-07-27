417e18a3e9b99b4cdc6ce71ef710bf001b061b55
Laurent Jouanneau <dev@ljouanneau.com>
2026-07-27 12:19:28 +0200

    Update webpack

--- assets/webpack.dev.js
+++ assets/webpack.dev.js
@@ -3,7 +3,7 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
     mode: 'development',
-    devtool: 'eval-source-map',
+    devtool: 'cheap-source-map',
     optimization: {
         minimize: false
     }
